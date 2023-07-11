90796418e21ab181a766d21b6c16c380629bf68b
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-07-11 18:06:11 +0100

    Integration test for TaskList and 'funding info' task
    Our TaskList will initially have one section, with one task:
    
    - Section: Area and funding
      - Task (and page): Funding information
    
    The "Funding information" task/page will have one question.

---
+++ integration_tests/tests/apply/view_tasklist.cy.ts
@@ -0,0 +1,73 @@
+//  Feature: Referrer views tasklist
+//    So that I can access the individual tasks in the application journey
+//    As a referrer
+//    I want to view a list of form tasks, grouped into sections
+
+//  Scenario: Sees link to task within its section
+//    Given there is a section with a task
+//    And I am logged in
+//    When I create an application
+//    Then I should see the task listed within the section
+//    And the task should link to its first page
+
+import IndexPage from '../../pages'
+import { personFactory, applicationFactory } from '../../../server/testutils/factories/index'
+
+context('New', () => {
+  const person = personFactory.build({})
+  const application = applicationFactory.build({})
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+  })
+
+  beforeEach(() => {
+    // Given I am logged in
+    //---------------------
+    cy.signIn()
+    cy.task('stubApplications', [])
+
+    // When I create an application
+    //------------------------------
+
+    //  visit the start page
+    IndexPage.visit()
+
+    //  click on the start button
+    cy.get('[role="button"]').click()
+
+    //  search for a CRN
+    cy.task('stubFindPerson', {
+      person,
+    })
+    cy.task('stubCreateApplication', { application })
+    cy.task('stubApplicationGet', { application })
+    cy.get('#crn').type(person.crn)
+    cy.get('button').contains('Save and continue').click()
+  })
+
+  // Then I should see the task listed within the section
+  // ----------------------------------------------------
+  it('shows the task listed within the section', () => {
+    // I'm on the task list page
+    cy.get('h2').contains('Application incomplete')
+
+    // I see the expected SECTION
+    cy.get('.app-task-list__section').contains('Area and funding')
+
+    // I see the expected TASK
+    cy.get('.app-task-list__task-name').contains('Funding information for CAS-2 placement')
+  })
+
+  // And the task should link to its first page
+  //-------------------------------------------
+  it('offers a link to the first page of the task', () => {
+    // I see a hyperlink which could be followed
+    cy.get('.section .task:first a')
+      .should('have.class', 'active')
+      .and('have.attr', 'href')
+      .and('include', 'tasks/area-and-funding/pages/funding-information')
+  })
+})
