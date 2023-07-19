1d4f01581fbda89ba3d5c847a26efa1d0f64f716
Harriet H-W <harriethw@gmail.com>
2023-07-19 16:04:36 +0100

    `validation.ts` unit test coverage

--- server/utils/validation.test.ts
+++ server/utils/validation.test.ts
@@ -1,8 +1,146 @@
 import { Request, Response } from 'express'
 import { createMock } from '@golevelup/ts-jest'
 import type { ErrorMessages, ErrorSummary } from '@approved-premises/ui'
-import { catchAPIErrorOrPropogate, fetchErrorsAndUserInput } from './validation'
-import { TaskListAPIError } from './errors'
+import { SanitisedError } from 'server/sanitisedError'
+import TaskListPage from 'server/form-pages/taskListPage'
+import {
+  catchAPIErrorOrPropogate,
+  catchValidationErrorOrPropogate,
+  fetchErrorsAndUserInput,
+  firstFlashItem,
+} from './validation'
+import { TaskListAPIError, ValidationError } from './errors'
+import errorLookups from '../i18n/en/errors.json'
+
+jest.mock('../i18n/en/errors.json', () => {
+  return {
+    fundingSource: {
+      empty: 'You must choose a funding source',
+    },
+  }
+})
+
+describe('catchValidationErrorOrPropogate', () => {
+  const request = createMock<Request>({})
+  const response = createMock<Response>()
+
+  const expectedErrors = {
+    fundingSource: { text: errorLookups.fundingSource.empty, attributes: { 'data-cy-error-fundingSource': true } },
+  }
+
+  const expectedErrorSummary = [{ text: errorLookups.fundingSource.empty, href: '#fundingSource' }]
+
+  beforeEach(() => {
+    request.body = {
+      some: 'field',
+    }
+  })
+
+  it('sets the errors and request body as flash messages and redirects back to the form', () => {
+    const error = createMock<SanitisedError>({
+      data: {
+        'invalid-params': [
+          {
+            propertyName: '$.fundingSource',
+            errorType: 'empty',
+          },
+        ],
+      },
+    })
+
+    catchValidationErrorOrPropogate(request, response, error, 'some/url')
+
+    expect(request.flash).toHaveBeenCalledWith('errors', expectedErrors)
+    expect(request.flash).toHaveBeenCalledWith('errorSummary', expectedErrorSummary)
+    expect(request.flash).toHaveBeenCalledWith('userInput', request.body)
+
+    expect(response.redirect).toHaveBeenCalledWith('some/url')
+  })
+
+  it('sets a generic error and redirects back to the form', () => {
+    const error = createMock<SanitisedError>({
+      data: {
+        detail: 'Some generic error',
+        'invalid-params': [],
+      },
+    })
+
+    catchValidationErrorOrPropogate(request, response, error, 'some/url')
+
+    expect(request.flash).toHaveBeenCalledWith('errorSummary', { text: 'Some generic error' })
+    expect(request.flash).toHaveBeenCalledWith('userInput', request.body)
+
+    expect(response.redirect).toHaveBeenCalledWith('some/url')
+  })
+
+  it('gets errors from a ValidationError type', () => {
+    const error = new ValidationError<TaskListPage>({
+      data: {
+        crn: 'You must enter a valid crn',
+        error: 'You must enter a valid arrival date',
+      },
+    })
+
+    catchValidationErrorOrPropogate(request, response, error, 'some/url')
+
+    expect(request.flash).toHaveBeenCalledWith('errors', expectedErrors)
+    expect(request.flash).toHaveBeenCalledWith('errorSummary', expectedErrorSummary)
+    expect(request.flash).toHaveBeenCalledWith('userInput', request.body)
+
+    expect(response.redirect).toHaveBeenCalledWith('some/url')
+  })
+
+  it('throws the error if the error is not the type we expect', () => {
+    const err = new Error('Some unhandled error')
+    err.name = 'SomeUnhandledError'
+    err.stack = 'STACK_GOES_HERE'
+
+    const result = () => catchValidationErrorOrPropogate(request, response, err, 'some/url')
+
+    expect(result).toThrowError(err)
+    expect(result).toThrowError(
+      expect.objectContaining({
+        message: 'Some unhandled error',
+        name: 'SomeUnhandledError',
+        stack: 'STACK_GOES_HERE',
+      }),
+    )
+  })
+
+  it('throws an error if the property is not found in the error lookup', () => {
+    const error = createMock<SanitisedError>({
+      data: {
+        'invalid-params': [
+          {
+            propertyName: '$.foo',
+            errorType: 'bar',
+          },
+        ],
+      },
+    })
+
+    expect(() => catchValidationErrorOrPropogate(request, response, error, 'some/url')).toThrowError(
+      'Cannot find a translation for an error at the path $.foo',
+    )
+  })
+
+  it('throws an error if the error type is not found in the error lookup', () => {
+    const error = createMock<SanitisedError>({
+      data: {
+        'invalid-params': [
+          {
+            propertyName: '$.fundingSource',
+            errorType: 'invalid',
+          },
+        ],
+      },
+    })
+
+    expect(() => catchValidationErrorOrPropogate(request, response, error, 'some/url')).toThrowError(
+      'Cannot find a translation for an error at the path $.fundingSource with the type invalid',
+    )
+  })
+})
 
 describe('catchAPIErrorOrPropogate', () => {
   const request = createMock<Request>({ headers: { referer: 'foo/bar' } })
@@ -24,6 +162,14 @@ describe('catchAPIErrorOrPropogate', () => {
     ])
 
     expect(response.redirect).toHaveBeenCalledWith(request.headers.referer)
+  })
+
+  it('throws the error if not of type TaskListAPIError', () => {
+    const error = Error('some unhandled error')
+
+    const result = () => catchAPIErrorOrPropogate(request, response, error)
+
+    expect(result).toThrowError(error)
   })
 })
 
@@ -61,5 +207,30 @@ describe('fetchErrorsAndUserInput', () => {
     const result = fetchErrorsAndUserInput(request)
 
     expect(result).toEqual({ errors, errorSummary, userInput, errorTitle })
+  })
+})
+
+describe('firstFlashItem', () => {
+  describe('when there is an item', () => {
+    it('returns the first flash item', () => {
+      const request = createMock<Request>({})
+      const flashMessage = 'flash message'
+      ;(request.flash as jest.Mock).mockImplementation(() => [flashMessage])
+
+      const result = firstFlashItem(request, 'key')
+
+      expect(result).toBe(flashMessage)
+    })
+  })
+
+  describe('when there is nothing in the flash', () => {
+    it('returns undefined', () => {
+      const request = createMock<Request>({})
+      ;(request.flash as jest.Mock).mockImplementation(() => null)
+
+      const result = firstFlashItem(request, 'key')
+
+      expect(result).toBe(undefined)
+    })
   })
 })
