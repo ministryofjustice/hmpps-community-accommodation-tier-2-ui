b7b185ee3c3253d649b7db8fb5448d21e305f831
Harriet H-W <harriethw@gmail.com>
2023-07-31 15:28:36 +0100

    add audit events to all routes
    This adds the audit events, following CAS3 and CAS1 process. There is
    also some refactoring going on:
        * using the `actions` utility function
        to build the `get` etc. actions. This allows us to abstract those
        functions away and have shared way of building them.
        * putting all the `applicationsController` routes in the `apply.ts`
        file
        * adding an ignore command to the routes files so that they are not
        included in unit coverage, like the other CAS projects. We wouldn't
        get much value from unit testing these files.

--- server/routes/apply.ts
+++ server/routes/apply.ts
@@ -1,25 +1,52 @@
 /* istanbul ignore file */
 
-import type { Router, RequestHandler } from 'express'
+import type { Router } from 'express'
 import Apply from '../form-pages/apply'
 
 import type { Controllers } from '../controllers'
 import paths from '../paths/apply'
-import asyncMiddleware from '../middleware/asyncMiddleware'
+import { Services } from '../services'
+import { actions } from './utils'
 
-export default function applyRoutes(controllers: Controllers, router: Router): Router {
+export default function applyRoutes(controllers: Controllers, router: Router, services: Services): Router {
   const { pages } = Apply
-  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const put = (path: string | string[], handler: RequestHandler) => router.put(path, asyncMiddleware(handler))
+  const { get, put } = actions(router, services.auditService)
 
-  const { pagesController } = controllers
+  const { applicationsController, pagesController } = controllers
+
+  get(paths.applications.new.pattern, applicationsController.new(), { auditEvent: 'VIEW_APPLICATION_NEW' })
+  get(paths.applications.index.pattern, applicationsController.index(), { auditEvent: 'VIEW_APPLICATIONS_LIST' })
+  get(paths.applications.show.pattern, applicationsController.show(), { auditEvent: 'VIEW_APPLICATION_START' })
 
   Object.keys(pages).forEach((taskKey: string) => {
     Object.keys(pages[taskKey]).forEach((pageKey: string) => {
       const { pattern } = paths.applications.show.path(`tasks/${taskKey}/pages/${pageKey}`)
 
-      get(pattern, pagesController.show(taskKey, pageKey))
-      put(pattern, pagesController.update(taskKey, pageKey))
+      get(pattern, pagesController.show(taskKey, pageKey), {
+        auditEvent: 'VIEW_APPLICATION_PAGE',
+        additionalMetadata: { task: taskKey, page: pageKey },
+      })
+      put(pattern, pagesController.update(taskKey, pageKey), {
+        auditEvent: `UPDATE_APPLICATION_PAGE`,
+        additionalMetadata: { task: taskKey, page: pageKey },
+        redirectAuditEventSpecs: [
+          {
+            // If we redirect to the same page, the user has hit an error
+            path: pattern,
+            auditEvent: 'UPDATE_APPLICATION_PAGE_FAILURE',
+          },
+          {
+            // If we redirect to the task list page, the application updated successfully
+            path: paths.applications.show.pattern,
+            auditEvent: 'UPDATE_APPLICATION_PAGE_SUCCESS',
+          },
+          {
+            // If we redirect to another application page, the application updated successfully
+            path: paths.applications.pages.show.pattern,
+            auditEvent: 'UPDATE_APPLICATION_PAGE_SUCCESS',
+          },
+        ],
+      })
     })
   })
 
