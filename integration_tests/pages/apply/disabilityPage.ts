35c407e0477911c55c27c32142b5fe164a4fdf0f
Patrick Fleming <patrick.fleming@dwx.com>
2023-08-01 10:25:46 +0100

    Update integration tests for disability form page

---
+++ integration_tests/pages/apply/disabilityPage.ts
@@ -0,0 +1,19 @@
+import { Cas2Application as Application } from '../../../server/@types/shared/models/Cas2Application'
+import ApplyPage from './applyPage'
+
+export default class DisabilityPage extends ApplyPage {
+  constructor(private readonly application: Application) {
+    super(
+      `Does ${application.person.name} have a disability?`,
+      application,
+      'equality-and-diversity-monitoring',
+      'disability',
+    )
+  }
+
+  enterOtherDisabilityType(): void {
+    this.checkRadioButtonFromPageBody('hasDisability')
+    this.checkCheckboxByLabel('other')
+    this.getTextInputByIdAndEnterDetails('otherDisability', 'a different disability')
+  }
+}
