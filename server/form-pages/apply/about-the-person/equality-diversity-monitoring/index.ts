337f0137afc2de0cd6adfab387557de3e5f6519e
Harriet H-W <harriethw@gmail.com>
2023-08-02 17:01:59 +0100

    add the `sexAndGender` page to the task list

--- server/form-pages/apply/about-the-person/equality-diversity-monitoring/index.ts
+++ server/form-pages/apply/about-the-person/equality-diversity-monitoring/index.ts
@@ -3,10 +3,11 @@
 import { Task } from '../../../utils/decorators'
 import WillAnswer from './willAnswer'
 import Disability from './disability'
+import SexAndGender from './sexAndGender'
 
 @Task({
   name: 'Complete equality and diversity monitoring',
   slug: 'equality-and-diversity-monitoring',
-  pages: [WillAnswer, Disability],
+  pages: [WillAnswer, Disability, SexAndGender],
 })
 export default class EqualityAndDiversityMonitoring {}
