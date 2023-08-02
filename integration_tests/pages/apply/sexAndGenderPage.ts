6a2b050e5f9e5869c47f183ed0c557030cab0e69
Harriet H-W <harriethw@gmail.com>
2023-08-02 17:02:55 +0100

    add integration tests for the new `sexAndGender` page

---
+++ integration_tests/pages/apply/sexAndGenderPage.ts
@@ -0,0 +1,16 @@
+import { Cas2Application as Application } from '../../../server/@types/shared/models/Cas2Application'
+import ApplyPage from './applyPage'
+
+export default class SexAndGenderPage extends ApplyPage {
+  constructor(private readonly application: Application) {
+    super('Sex and gender identity', application, 'equality-and-diversity-monitoring', 'sex-and-gender')
+  }
+
+  selectSex(): void {
+    this.checkRadioByNameAndValue('sex', 'female')
+  }
+
+  confirmGenderIdentity(): void {
+    this.checkRadioByNameAndValue('gender', 'no')
+  }
+}
