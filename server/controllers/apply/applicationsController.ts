114a8bf7c99e758350bdb22d93c2c288468dd6c5
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-08-03 10:39:15 +0100

    Render ineligible page if eligibility has been 'denied'

--- server/controllers/apply/applicationsController.ts
+++ server/controllers/apply/applicationsController.ts
@@ -2,7 +2,11 @@ import { Request, RequestHandler, Response } from 'express'
 import PersonService from '../../services/personService'
 import { fetchErrorsAndUserInput } from '../../utils/validation'
 import ApplicationService from '../../services/applicationService'
-import { eligibilityQuestionIsAnswered, firstPageOfBeforeYouStartSection } from '../../utils/applications/utils'
+import {
+  eligibilityIsDenied,
+  eligibilityIsConfirmed,
+  firstPageOfBeforeYouStartSection,
+} from '../../utils/applications/utils'
 import TaskListService from '../../services/taskListService'
 import paths from '../../paths/apply'
 
@@ -32,9 +36,13 @@ export default class ApplicationsController {
     return async (req: Request, res: Response) => {
       const application = await this.applicationService.findApplication(req.user.token, req.params.id)
 
-      if (eligibilityQuestionIsAnswered(application)) {
+      if (eligibilityIsConfirmed(application)) {
         const taskList = new TaskListService(application)
         return res.render('applications/taskList', { application, taskList })
+      }
+
+      if (eligibilityIsDenied(application)) {
+        return res.render('applications/ineligible', { application })
       }
 
       return res.redirect(firstPageOfBeforeYouStartSection(application))
