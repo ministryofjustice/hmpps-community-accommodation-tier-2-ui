b45cbb93a2229029ad33916773026f8ed86f006a
Harriet H-W <harriethw@gmail.com>
2023-08-03 15:04:16 +0100

    add sexualOrientation page to task

--- server/form-pages/apply/about-the-person/equality-diversity-monitoring/index.ts
+++ server/form-pages/apply/about-the-person/equality-diversity-monitoring/index.ts
@@ -4,10 +4,11 @@ import { Task } from '../../../utils/decorators'
 import WillAnswer from './willAnswer'
 import Disability from './disability'
 import SexAndGender from './sexAndGender'
+import SexualOrientation from './sexualOrientation'
 
 @Task({
   name: 'Complete equality and diversity monitoring',
   slug: 'equality-and-diversity-monitoring',
-  pages: [WillAnswer, Disability, SexAndGender],
+  pages: [WillAnswer, Disability, SexAndGender, SexualOrientation],
 })
 export default class EqualityAndDiversityMonitoring {}
