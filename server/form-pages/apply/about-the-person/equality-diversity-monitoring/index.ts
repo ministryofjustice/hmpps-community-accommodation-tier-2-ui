56f3881dad4ae0f9bf01870ca95d65f567b0286c
Harriet H-W <harriethw@gmail.com>
2023-08-07 11:44:30 +0100

    mixed-background: add page to Task

--- server/form-pages/apply/about-the-person/equality-diversity-monitoring/index.ts
+++ server/form-pages/apply/about-the-person/equality-diversity-monitoring/index.ts
@@ -7,10 +7,11 @@ import SexAndGender from './sexAndGender'
 import SexualOrientation from './sexualOrientation'
 import EthnicGroup from './ethnicGroup'
 import WhiteBackground from './whiteBackground'
+import MixedBackground from './mixedBackground'
 
 @Task({
   name: 'Complete equality and diversity monitoring',
   slug: 'equality-and-diversity-monitoring',
-  pages: [WillAnswer, Disability, SexAndGender, SexualOrientation, EthnicGroup, WhiteBackground],
+  pages: [WillAnswer, Disability, SexAndGender, SexualOrientation, EthnicGroup, WhiteBackground, MixedBackground],
 })
 export default class EqualityAndDiversityMonitoring {}
