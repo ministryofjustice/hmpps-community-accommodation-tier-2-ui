903bffa64b843efff90503fcb01afd49a3864e64
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-08-09 12:09:25 +0100

    Describe completion of 'Substance misuse' page

--- integration_tests/pages/apply/risks-and-needs/substanceMisusePage.ts
+++ integration_tests/pages/apply/risks-and-needs/substanceMisusePage.ts
@@ -19,4 +19,20 @@ export default class SubstanceMisusePage extends ApplyPage {
       }),
     )
   }
+
+  describeIllegalSubstanceUse = (): void => {
+    this.checkRadioByNameAndValue('usesIllegalSubstances', 'yes')
+    this.getTextInputByIdAndEnterDetails('substanceMisuseHistory', 'Heroin user')
+    this.getTextInputByIdAndEnterDetails('substanceMisuseDetail', 'Injects daily')
+  }
+
+  nameDrugAndAlcholService = (): void => {
+    this.checkRadioByNameAndValue('engagedWithDrugAndAlcoholService', 'yes')
+    this.getTextInputByIdAndEnterDetails('drugAndAlcoholServiceDetail', 'The Drugs Project')
+  }
+
+  provideSubsituteMedicationDetails = (): void => {
+    this.checkRadioByNameAndValue('requiresSubstituteMedication', 'yes')
+    this.getTextInputByIdAndEnterDetails('substituteMedicationDetail', 'Methadone')
+  }
 }
