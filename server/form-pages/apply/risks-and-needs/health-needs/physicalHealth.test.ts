0cdc4e3cf579e66b2421c23d4f6610b8b809f555
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-08-14 09:38:35 +0100

    Add questions to 'Physical health' page
    The exact copy of the questions will be set in the next
    commit where we'll define the expected `response()`.

--- server/form-pages/apply/risks-and-needs/health-needs/physicalHealth.test.ts
+++ server/form-pages/apply/risks-and-needs/health-needs/physicalHealth.test.ts
@@ -16,6 +16,56 @@ describe('PhysicalHealth', () => {
   itShouldHaveNextValue(new PhysicalHealth({}, application), 'mental-health')
   itShouldHavePreviousValue(new PhysicalHealth({}, application), 'substance-misuse')
 
+  describe('Questions', () => {
+    const page = new PhysicalHealth({}, application)
+
+    describe('hasPhyHealthNeeds', () => {
+      it('has a question', () => {
+        expect(page.questions.hasPhyHealthNeeds.question).toBeDefined()
+      })
+      it('has two follow-up questions', () => {
+        expect(page.questions.hasPhyHealthNeeds.needsDetail.question).toBeDefined()
+        expect(page.questions.hasPhyHealthNeeds.canClimbStairs.question).toBeDefined()
+      })
+    })
+
+    describe('isReceivingTreatment', () => {
+      it('has a question', () => {
+        expect(page.questions.isReceivingTreatment.question).toBeDefined()
+      })
+      it('has a follow-up question', () => {
+        expect(page.questions.isReceivingTreatment.treatmentDetail.question).toBeDefined()
+      })
+    })
+
+    describe('hasPhyHealthMedication', () => {
+      it('has a question', () => {
+        expect(page.questions.hasPhyHealthMedication.question).toBeDefined()
+      })
+      it('has a follow-up question', () => {
+        expect(page.questions.hasPhyHealthMedication.medicationDetail.question).toBeDefined()
+      })
+    })
+
+    describe('canLiveIndependently', () => {
+      it('has a question', () => {
+        expect(page.questions.canLiveIndependently.question).toBeDefined()
+      })
+      it('has a follow-up question', () => {
+        expect(page.questions.canLiveIndependently.indyLivingDetail.question).toBeDefined()
+      })
+    })
+
+    describe('requiresAdditionalSupport', () => {
+      it('has a question', () => {
+        expect(page.questions.requiresAdditionalSupport.question).toBeDefined()
+      })
+      it('has a follow-up question', () => {
+        expect(page.questions.requiresAdditionalSupport.addSupportDetail.question).toBeDefined()
+      })
+    })
+  })
+
   describe('response', () => {
     it('not implemented', () => {
       const page = new PhysicalHealth({}, application)
--- server/form-pages/apply/risks-and-needs/health-needs/physicalHealth.ts
+++ server/form-pages/apply/risks-and-needs/health-needs/physicalHealth.ts
@@ -12,6 +12,42 @@ type PhysicalHealthBody = Record<string, never>
 export default class PhysicalHealth implements TaskListPage {
   title = `Physical health needs for ${this.application.person.name}`
 
+  questions = {
+    hasPhyHealthNeeds: {
+      question: 'Physical health needs?',
+      needsDetail: {
+        question: 'Details',
+      },
+      canClimbStairs: {
+        question: 'Can climb stairs',
+      },
+    },
+    isReceivingTreatment: {
+      question: 'Is receiving treatment?',
+      treatmentDetail: {
+        question: 'Treatment detail',
+      },
+    },
+    hasPhyHealthMedication: {
+      question: 'Receives medication?',
+      medicationDetail: {
+        question: 'Medication detail',
+      },
+    },
+    canLiveIndependently: {
+      question: 'Can live independently?',
+      indyLivingDetail: {
+        question: 'Independent living detail',
+      },
+    },
+    requiresAdditionalSupport: {
+      question: 'Requires additional support?',
+      addSupportDetail: {
+        question: 'Additional support details',
+      },
+    },
+  }
+
   body: PhysicalHealthBody
 
   constructor(
