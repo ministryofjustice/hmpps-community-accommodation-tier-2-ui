275ea13b6ba8ddd0bb0a7ff0e9763b507f8b4e02
Patrick Fleming <patrick.fleming@dwx.com>
2023-08-15 11:28:17 +0100

    Add care leaver page

---
+++ server/form-pages/apply/about-the-person/equality-diversity-monitoring/careLeaver.test.ts
@@ -0,0 +1,96 @@
+import { personFactory, applicationFactory } from '../../../../testutils/factories/index'
+import CareLeaver from './careLeaver'
+import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../../shared-examples'
+
+describe('CareLeaver', () => {
+  const application = applicationFactory.build({ person: personFactory.build({ name: 'Roger Smith' }) })
+
+  describe('title', () => {
+    it('personalises the page title', () => {
+      const page = new CareLeaver({}, application)
+
+      expect(page.title).toEqual('Equality and diversity questions for Roger Smith')
+    })
+  })
+
+  describe('question', () => {
+    it('personalises the question', () => {
+      const page = new CareLeaver({ isCareLeaver: 'yes' }, application)
+
+      expect(page.questions).toEqual({
+        isCareLeaver: 'Is Roger Smith a care leaver?',
+      })
+    })
+  })
+
+  itShouldHaveNextValue(new CareLeaver({ isCareLeaver: 'yes' }, application), '')
+  itShouldHavePreviousValue(new CareLeaver({ isCareLeaver: 'yes' }, application), 'military-veteran')
+
+  describe('response', () => {
+    it('Adds selected option to page response in _translated_ form', () => {
+      const page = new CareLeaver(
+        {
+          isCareLeaver: 'yes',
+        },
+        application,
+      )
+
+      expect(page.response()).toEqual({
+        'Is Roger Smith a care leaver?': 'Yes',
+      })
+    })
+  })
+
+  describe('items', () => {
+    it('returns the radio with the expected label text', () => {
+      const page = new CareLeaver(
+        {
+          isCareLeaver: 'yes',
+        },
+        application,
+      )
+
+      expect(page.items()).toEqual([
+        {
+          value: 'yes',
+          text: 'Yes',
+          checked: true,
+        },
+        {
+          value: 'no',
+          text: 'No',
+          checked: false,
+        },
+        {
+          divider: 'or',
+        },
+        {
+          value: 'dontKnow',
+          text: `I don't know`,
+          checked: false,
+        },
+      ])
+    })
+  })
+
+  describe('errors', () => {
+    it('does not return an error for valid answers', () => {
+      const page = new CareLeaver(
+        {
+          isCareLeaver: 'no',
+        },
+        application,
+      )
+
+      expect(page.errors()).toEqual({})
+    })
+
+    it('should return errors when no answer given', () => {
+      const page = new CareLeaver({}, application)
+
+      expect(page.errors()).toEqual({
+        isCareLeaver: `Choose either Yes, No or I don't know`,
+      })
+    })
+  })
+})
---
+++ server/form-pages/apply/about-the-person/equality-diversity-monitoring/careLeaver.ts
@@ -0,0 +1,66 @@
+import type { TaskListErrors, YesNoOrIDK } from '@approved-premises/ui'
+import { Cas2Application as Application } from '@approved-premises/api'
+import { Page } from '../../../utils/decorators'
+import TaskListPage from '../../../taskListPage'
+import { convertKeyValuePairToRadioItems } from '../../../../utils/formUtils'
+import errorLookups from '../../../../i18n/en/errors.json'
+
+export const options = {
+  yes: 'Yes',
+  no: 'No',
+  dontKnow: `I don't know`,
+}
+
+export type CareLeaverBody = {
+  isCareLeaver: YesNoOrIDK
+}
+
+@Page({
+  name: 'care-leaver',
+  bodyProperties: ['isCareLeaver'],
+})
+export default class CareLeaver implements TaskListPage {
+  title = `Equality and diversity questions for ${this.application.person.name}`
+
+  questions = {
+    isCareLeaver: `Is ${this.application.person.name} a care leaver?`,
+  }
+
+  body: CareLeaverBody
+
+  constructor(
+    body: Partial<CareLeaverBody>,
+    private readonly application: Application,
+  ) {}
+
+  previous() {
+    return 'military-veteran'
+  }
+
+  next() {
+    return ''
+  }
+
+  errors() {
+    const errors: TaskListErrors<this> = {}
+    if (!this.body.isCareLeaver) {
+      errors.isCareLeaver = errorLookups.isCareLeaver.empty
+    }
+    return errors
+  }
+
+  response() {
+    return {
+      [this.questions.isCareLeaver]: options[this.body.isCareLeaver],
+    }
+  }
+
+  items() {
+    const items = convertKeyValuePairToRadioItems(options, this.body.isCareLeaver)
+
+    const dontKnow = items.pop()
+    items.push({ divider: 'or' })
+
+    return [...items, { ...dontKnow }]
+  }
+}
