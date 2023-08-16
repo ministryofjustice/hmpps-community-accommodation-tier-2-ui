b27e7944e206fafac7f0d814f8c368fa057f3a89
Patrick Fleming <patrick.fleming@dwx.com>
2023-08-16 13:45:56 +0100

    Add military veteran page

---
+++ server/form-pages/apply/about-the-person/equality-diversity-monitoring/militaryVeteran.ts
@@ -0,0 +1,66 @@
+import type { TaskListErrors, YesNoOrIDK } from '@approved-premises/ui'
+import { Cas2Application as Application } from '@approved-premises/api'
+import { Page } from '../../../utils/decorators'
+import TaskListPage from '../../../taskListPage'
+import { convertKeyValuePairToRadioItems } from '../../../../utils/formUtils'
+import errorLookups from '../../../../i18n/en/errors.json'
+
+export const options = {
+  yes: 'Yes',
+  no: 'No',
+  dontKnow: `I don't know`,
+}
+
+export type MilitaryVeteranBody = {
+  isVeteran: YesNoOrIDK
+}
+
+@Page({
+  name: 'military-veteran',
+  bodyProperties: ['isVeteran'],
+})
+export default class MilitaryVeteran implements TaskListPage {
+  title = `Equality and diversity questions for ${this.application.person.name}`
+
+  questions = {
+    isVeteran: `Is ${this.application.person.name} a military veteran?`,
+  }
+
+  body: MilitaryVeteranBody
+
+  constructor(
+    body: Partial<MilitaryVeteranBody>,
+    private readonly application: Application,
+  ) {}
+
+  previous() {
+    return 'religion'
+  }
+
+  next() {
+    return ''
+  }
+
+  errors() {
+    const errors: TaskListErrors<this> = {}
+    if (!this.body.isVeteran) {
+      errors.isVeteran = errorLookups.isVeteran.empty
+    }
+    return errors
+  }
+
+  response() {
+    return {
+      [this.questions.isVeteran]: options[this.body.isVeteran],
+    }
+  }
+
+  items() {
+    const items = convertKeyValuePairToRadioItems(options, this.body.isVeteran)
+
+    const dontKnow = items.pop()
+    items.push({ divider: 'or' })
+
+    return [...items, { ...dontKnow }]
+  }
+}
