0becbde0c4ea8cd0cae66bbb3b1733fb9bf30537
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-08-18 07:09:07 +0100

    Verify that '(optional)' flag is shown on question label
    We add an `fieldIsOptional` integration test helper so
    we can verify that the optional flag has been set as
    expected.
    
    Now any text area can have its label given an 'optional'
    flag without having to corrupt the question's title.
    
    We will probably soon need this "optional flag" feature to
    `formPageInput` also, but I held off from doing this now as
    we don't have a use-case with which to verify that it's
    working.

--- integration_tests/pages/apply/risks-and-needs/utils.ts
+++ integration_tests/pages/apply/risks-and-needs/utils.ts
@@ -1,3 +1,7 @@
 export const pageIsActiveInNavigation = (linkText: string): void => {
   cy.get('.moj-side-navigation__item--active a').contains(linkText)
 }
+
+export const fieldIsOptional = (labelId: string): void => {
+  cy.get(`label[for="${labelId}"]`).contains('(optional)')
+}
