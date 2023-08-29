7fcefaa0939a38962ca88c04fedace0e0a3ea2f8
Harriet H-W <harriethw@gmail.com>
2023-08-29 10:48:30 +0100

    import shared API types for OASysRiskToSelf

---
+++ server/@types/shared/models/OASysRiskToSelf.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ArrayOfOASysRiskToSelfQuestions } from './ArrayOfOASysRiskToSelfQuestions';
+import type { OASysAssessmentId } from './OASysAssessmentId';
+import type { OASysAssessmentState } from './OASysAssessmentState';
+
+export type OASysRiskToSelf = {
+    assessmentId: OASysAssessmentId;
+    assessmentState: OASysAssessmentState;
+    dateStarted: string;
+    dateCompleted?: string;
+    riskToSelf: ArrayOfOASysRiskToSelfQuestions;
+};
+
