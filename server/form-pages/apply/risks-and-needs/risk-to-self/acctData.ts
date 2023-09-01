64900b12678e3415d53fc8a51759773de8c0c30d
Harriet H-W <harriethw@gmail.com>
2023-09-01 09:41:47 +0100

    use custom form

--- server/form-pages/apply/risks-and-needs/risk-to-self/acctData.ts
+++ server/form-pages/apply/risks-and-needs/risk-to-self/acctData.ts
@@ -4,12 +4,12 @@ import { Page } from '../../../utils/decorators'
 import TaskListPage from '../../../taskListPage'
 
 type AcctBody = {
-  acctData: { acctDetail: string }[]
+  acctDetail: string
 }
 
 @Page({
   name: 'acct-data',
-  bodyProperties: ['acctData'],
+  bodyProperties: ['acctDetail'],
 })
 export default class AcctData implements TaskListPage {
   title = 'Add an ACCT entry'
@@ -22,21 +22,45 @@ export default class AcctData implements TaskListPage {
     },
   }
 
-  existingAccts: { acctDetail: string }[]
+  taskData: string
+
+  taskName = 'risk-to-self'
 
   constructor(
     body: Partial<AcctBody>,
     private readonly application: Application,
+    taskData: string,
   ) {
     console.log('constructor', application.data['risk-to-self'])
     console.log('body in page', body)
-    let existingAccts
-    if (application.data['risk-to-self']['acct-data']) {
-      existingAccts = application.data['risk-to-self']['acct-data'].acctData
-      console.log('existing accts', existingAccts)
-    }
-    this.existingAccts = existingAccts
+    this.taskData = taskData
     this.body = body as AcctBody
+  }
+
+  static async initialize(
+    body: Partial<AcctBody>,
+    application: Cas2Application,
+    token: string,
+    dataServices: DataServices,
+  ) {
+    let oasys
+    let taskDataJson
+
+    if (!application.data['risk-to-self']) {
+      try {
+        oasys = await dataServices.personService.getOasysRiskToSelf(token, application.person.crn)
+
+        taskDataJson = JSON.stringify(RiskToSelfGuidance.getTaskData(oasys))
+      } catch (e) {
+        if (e.status === 404) {
+          oasys = null
+        } else {
+          throw e
+        }
+      }
+      return new RiskToSelfGuidance(body, application, oasys, taskDataJson)
+    }
+    return new Vulnerability(application.data['risk-to-self'].vulnerability, application)
   }
 
   previous() {
