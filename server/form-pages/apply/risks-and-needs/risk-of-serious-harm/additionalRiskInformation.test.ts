919302fca1802a821a25aca4c507810e0d69679a
Harriet H-W <harriethw@gmail.com>
2023-09-15 11:04:51 +0100

    add additional information page for ROSH

--- server/form-pages/apply/risks-and-needs/risk-of-serious-harm/additionalRiskInformation.test.ts
+++ server/form-pages/apply/risks-and-needs/risk-of-serious-harm/additionalRiskInformation.test.ts
@@ -17,18 +17,46 @@ describe('AdditionalRiskInformation', () => {
   itShouldHavePreviousValue(new AdditionalRiskInformation({}, application), 'behaviour-notes')
 
   describe('response', () => {
-    it('not implemented', () => {
-      const page = new AdditionalRiskInformation({}, application)
+    it('returns the correct plain english responses for the questions', () => {
+      const page = new AdditionalRiskInformation(
+        {
+          hasAdditionalInformation: 'yes',
+          additionalInformationDetail: 'is at risk',
+        },
+        application,
+      )
 
-      expect(page.response()).toEqual({})
+      expect(page.response()).toEqual({
+        'Is there any other risk information for Roger Smith?': 'yes',
+        'Additional information': 'is at risk',
+      })
     })
   })
 
   describe('errors', () => {
-    it('not implemented', () => {
-      const page = new AdditionalRiskInformation({}, application)
+    describe('when answer data is valid', () => {
+      it('returns empty object if valid data', () => {
+        const page = new AdditionalRiskInformation({ hasAdditionalInformation: 'no' }, application)
+        expect(page.errors()).toEqual({})
+      })
+    })
 
-      expect(page.errors()).toEqual({})
+    describe('when they have not answered the has additional information question', () => {
+      it('returns an error', () => {
+        const page = new AdditionalRiskInformation({}, application)
+        expect(page.errors()).toEqual({
+          hasAdditionalInformation: 'Select whether there is any additional risk information',
+        })
+      })
+    })
+
+    describe('when they have answered Yes there is additional information but not provided any', () => {
+      it('returns an error', () => {
+        const page = new AdditionalRiskInformation({ hasAdditionalInformation: 'yes' }, application)
+        expect(page.errors()).toEqual({
+          additionalInformationDetail: 'Enter additional information for risk to others',
+        })
+      })
     })
   })
 })
