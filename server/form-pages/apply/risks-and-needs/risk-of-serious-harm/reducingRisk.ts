37050c3db9aa9faa2d35975358ac6d8260a82c61
Harriet H-W <harriethw@gmail.com>
2023-09-22 16:12:15 +0100

    add reducing risk to RoSH task

--- server/form-pages/apply/risks-and-needs/risk-of-serious-harm/reducingRisk.ts
+++ server/form-pages/apply/risks-and-needs/risk-of-serious-harm/reducingRisk.ts
@@ -3,12 +3,18 @@ import { Cas2Application as Application } from '@approved-premises/api'
 import { Page } from '../../../utils/decorators'
 import TaskListPage from '../../../taskListPage'
 import { nameOrPlaceholderCopy } from '../../../../utils/utils'
+import { getOasysImportDateFromApplication } from '../../../utils'
+import { convertKeyValuePairToCheckboxItems } from '../../../../utils/formUtils'
+import errorLookups from '../../../../i18n/en/errors.json'
 
-type ReducingRiskBody = Record<string, never>
+type ReducingRiskBody = {
+  factorsLikelyToReduceRisk: string
+  confirmation: string
+}
 
 @Page({
   name: 'reducing-risk',
-  bodyProperties: [],
+  bodyProperties: ['factorsLikelyToReduceRisk', 'confirmation'],
 })
 export default class ReducingRisk implements TaskListPage {
   documentTitle = 'Reducing risk for the person'
@@ -16,6 +22,18 @@ export default class ReducingRisk implements TaskListPage {
   title = `Reducing risk for ${nameOrPlaceholderCopy(this.application.person)}`
 
   body: ReducingRiskBody
+
+  questions = {
+    factorsLikelyToReduceRisk: {
+      question: 'What factors are likely to reduce risk?',
+      hint: 'Describe factors, actions and events which might reduce or contain the level of risk now and in the future.',
+    },
+    confirmation: {
+      question: 'I confirm this information is relevant and up to date.',
+    },
+  }
+
+  importDate = getOasysImportDateFromApplication(this.application, 'risk-of-serious-harm')
 
   constructor(
     body: Partial<ReducingRiskBody>,
@@ -35,12 +53,28 @@ export default class ReducingRisk implements TaskListPage {
   errors() {
     const errors: TaskListErrors<this> = {}
 
+    if (!this.body.confirmation) {
+      errors.confirmation = errorLookups.oasysConfirmation.empty
+    }
+    if (!this.body.factorsLikelyToReduceRisk) {
+      errors.factorsLikelyToReduceRisk = 'Enter the factors that are likely to increase risk'
+    }
+
     return errors
   }
 
   response() {
-    const response = {}
+    const response = {
+      [this.questions.factorsLikelyToReduceRisk.question]: this.body.factorsLikelyToReduceRisk,
+      [this.questions.confirmation.question]: this.body.confirmation,
+    }
 
     return response
+  }
+
+  items() {
+    return convertKeyValuePairToCheckboxItems({ confirmed: this.questions.confirmation.question }, [
+      this.body.confirmation,
+    ])
   }
 }
