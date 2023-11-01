c9bc78ee23eb7908a0bb61ec132de5b4dc66b0fc
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-11-01 12:05:52 +0000

    Add new SubmittedApplications controller within '/assess/' namespace
    Submitted applications will be displayed in the UI at:
    
    ```
    GET /assess/applications/{:applicationId}
    ```
    
    As the application has permanently transitioned from being:
    
    - 'in progress', using the mutable `data` structure, to being
    - 'submitted' and offering only the read-only `document`
    
    it makes sense to move from the `/apply/` url namespace to a
    new namespace -- `/assess/`.

---
+++ server/controllers/assess/submittedApplicationsController.test.ts
@@ -0,0 +1,48 @@
+import type { NextFunction, Request, Response } from 'express'
+import { DeepMocked, createMock } from '@golevelup/ts-jest'
+import { FullPerson } from '@approved-premises/api'
+
+import { submittedApplicationFactory } from '../../testutils/factories'
+import SubmittedApplicationsController from './submittedApplicationsController'
+import { SubmittedApplicationService } from '../../services'
+import paths from '../../paths/assess'
+
+describe('submittedApplicationsController', () => {
+  const token = 'SOME_TOKEN'
+
+  let request: DeepMocked<Request> = createMock<Request>({ user: { token } })
+  let response: DeepMocked<Response> = createMock<Response>({})
+  const next: DeepMocked<NextFunction> = jest.fn()
+
+  const submittedApplicationService = createMock<SubmittedApplicationService>({})
+
+  let submittedApplicationsController: SubmittedApplicationsController
+
+  const submittedApplication = submittedApplicationFactory.build()
+
+  beforeEach(() => {
+    submittedApplicationsController = new SubmittedApplicationsController(submittedApplicationService)
+
+    request = createMock<Request>({ user: { token } })
+    response = createMock<Response>({})
+  })
+
+  describe('show', () => {
+    it('renders the submitted application _show_ template', async () => {
+      submittedApplicationService.findApplication.mockResolvedValue(submittedApplication)
+      const person = submittedApplication.person as FullPerson
+
+      const requestHandler = submittedApplicationsController.show()
+      await requestHandler(request, response, next)
+
+      expect(paths.submittedApplications.show({ id: submittedApplication.id })).toEqual(
+        `/assess/applications/${submittedApplication.id}`,
+      )
+
+      expect(response.render).toHaveBeenCalledWith('assess/applications/show', {
+        application: submittedApplication,
+        pageHeading: `Application for ${person.nomsNumber}`,
+      })
+    })
+  })
+})
