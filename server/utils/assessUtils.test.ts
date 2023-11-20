90ac937a1383c6b86541eaa7efe349b352353d0d
Patrick Fleming <patrick.fleming@dwx.com>
2023-11-20 11:25:24 +0000

    Pre-select current status
    Where there is a current status, the status update form radio for that status will be checked when the user lands on the status update page.

--- server/utils/assessUtils.test.ts
+++ server/utils/assessUtils.test.ts
@@ -1,22 +1,23 @@
+import { statusUpdateFactory } from '../testutils/factories'
 import { applicationStatusRadios } from './assessUtils'
 
 describe('applicationStatusRadios', () => {
-  it('returns an array of radios', () => {
-    const statuses = [
-      {
-        id: 'f5cd423b-08eb-4efb-96ff-5cc6bb073905',
-        name: 'moreInfoRequested',
-        label: 'More information requested',
-        description: 'More information about the application has been requested from the POM (Prison Offender Manager.',
-      },
-      {
-        id: 'ba4d8432-250b-4ab9-81ec-7eb4b16e5dd1',
-        name: 'awaitingDecision',
-        label: 'Awaiting decision',
-        description: 'All information has been received and the application is awaiting assessment.',
-      },
-    ]
+  const statuses = [
+    {
+      id: 'f5cd423b-08eb-4efb-96ff-5cc6bb073905',
+      name: 'moreInfoRequested',
+      label: 'More information requested',
+      description: 'More information about the application has been requested from the POM (Prison Offender Manager.',
+    },
+    {
+      id: 'ba4d8432-250b-4ab9-81ec-7eb4b16e5dd1',
+      name: 'awaitingDecision',
+      label: 'Awaiting decision',
+      description: 'All information has been received and the application is awaiting assessment.',
+    },
+  ]
 
+  it('returns an array of radios', () => {
     const expected = [
       {
         value: 'moreInfoRequested',
@@ -24,6 +25,7 @@ describe('applicationStatusRadios', () => {
         hint: {
           text: 'More information about the application has been requested from the POM (Prison Offender Manager.',
         },
+        checked: false,
       },
       {
         value: 'awaitingDecision',
@@ -31,9 +33,35 @@ describe('applicationStatusRadios', () => {
         hint: {
           text: 'All information has been received and the application is awaiting assessment.',
         },
+        checked: true,
       },
     ]
 
-    expect(applicationStatusRadios(statuses)).toEqual(expected)
+    const previousStatuses = [statusUpdateFactory.build({ name: 'awaitingDecision' })]
+
+    expect(applicationStatusRadios(statuses, previousStatuses)).toEqual(expected)
+  })
+
+  it('does not check a radio if previousStatuses is empty array', () => {
+    const expected = [
+      {
+        value: 'moreInfoRequested',
+        text: 'More information requested',
+        hint: {
+          text: 'More information about the application has been requested from the POM (Prison Offender Manager.',
+        },
+        checked: false,
+      },
+      {
+        value: 'awaitingDecision',
+        text: 'Awaiting decision',
+        hint: {
+          text: 'All information has been received and the application is awaiting assessment.',
+        },
+        checked: false,
+      },
+    ]
+
+    expect(applicationStatusRadios(statuses, [])).toEqual(expected)
   })
 })
--- server/utils/assessUtils.ts
+++ server/utils/assessUtils.ts
@@ -1,9 +1,15 @@
-export const applicationStatusRadios = (statuses: Array<Record<string, string>>) => {
+import { Cas2StatusUpdate } from '@approved-premises/api'
+
+export const applicationStatusRadios = (
+  statuses: Array<Record<string, string>>,
+  previousStatuses: Array<Cas2StatusUpdate>,
+) => {
   return statuses.map(status => {
     return {
       value: status.name,
       text: status.label,
       hint: status?.description ? { text: status.description } : null,
+      checked: previousStatuses.length ? status.name === previousStatuses[0].name : false,
     }
   })
 }
