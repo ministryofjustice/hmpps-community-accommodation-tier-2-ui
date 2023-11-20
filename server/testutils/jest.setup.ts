dc51ed52d41a418f60e1364d131932ffacc67d78
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-11-20 11:55:26 +0000

    Tighten matching of /applications paths for PACT rewrites to OpenAPI spec
    We only want to insert `/cas2` for the "/applications"
    endpoints, not for others which include "application" such
    as `reference-data/application-status`.

--- server/testutils/jest.setup.ts
+++ server/testutils/jest.setup.ts
@@ -36,7 +36,7 @@ expect.extend({
       execSync(`
         if [ ! -f ${openAPIPath} ]; then
           curl -s "${openAPIUrl}" |
-          sed -E 's@/application@/cas2/application@g' |
+          sed -E 's@/applications@/cas2/applications@g' |
           sed -E 's@/submissions@/cas2/submissions@g' |
           sed -E 's@/people@/cas2/people@g' |
           sed -E 's@/reference-data/@/cas2/reference-data/@g' > ${openAPIPath}
