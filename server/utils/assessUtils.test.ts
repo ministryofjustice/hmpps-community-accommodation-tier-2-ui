4ae38e71884948736e4f174e772a6abcb0ca352d
Harriet H-W <harriethw@gmail.com>
2023-11-27 16:33:11 +0000

    add test coverage for assess utils

--- server/utils/assessUtils.test.ts
+++ server/utils/assessUtils.test.ts
@@ -15,6 +15,11 @@ describe('applicationStatusRadios', () => {
       label: 'Awaiting decision',
       description: 'All information has been received and the application is awaiting assessment.',
     },
+    {
+      id: 'cd4d8432-250b-4ab9-81ec-7eb4b16e5cc2',
+      name: 'example',
+      label: 'Example',
+    },
   ]
 
   it('returns an array of radios', () => {
@@ -34,6 +39,12 @@ describe('applicationStatusRadios', () => {
           text: 'All information has been received and the application is awaiting assessment.',
         },
         checked: true,
+      },
+      {
+        value: 'example',
+        text: 'Example',
+        hint: null,
+        checked: false,
       },
     ]
 
@@ -58,6 +69,12 @@ describe('applicationStatusRadios', () => {
         hint: {
           text: 'All information has been received and the application is awaiting assessment.',
         },
+        checked: false,
+      },
+      {
+        value: 'example',
+        text: 'Example',
+        hint: null,
         checked: false,
       },
     ]
