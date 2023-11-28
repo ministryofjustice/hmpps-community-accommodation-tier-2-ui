774fc54dc1ccf7dbc7cadb9c7164537ec398872d
Harriet H-W <harriethw@gmail.com>
2023-11-28 17:44:07 +0000

    add pncNumber to person

--- server/testutils/factories/person.ts
+++ server/testutils/factories/person.ts
@@ -11,6 +11,7 @@ export const fullPersonFactory = Factory.define<FullPerson>(() => ({
   sex: faker.helpers.arrayElement(['Male', 'Female', 'Other', 'Prefer not to say']),
   status: faker.helpers.arrayElement(['InCustody', 'InCommunity']),
   nomsNumber: `NOMS${faker.number.int({ min: 100, max: 999 })}`,
+  pncNumber: `PNC${faker.number.int({ min: 100, max: 999 })}`,
   nationality: faker.location.country(),
   religionOrBelief: faker.helpers.arrayElement(['Christian', 'Muslim', 'Jewish', 'Hindu', 'Buddhist', 'Sikh', 'None']),
   prisonName: `HMP ${faker.location.street()}`,
