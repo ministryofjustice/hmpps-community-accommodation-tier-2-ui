16d4edc66f97f3353665a78e0ae3aebab03739c0
Daniel Liburd <daniel.liburd@gmail.com>
2023-11-30 11:08:35 +0000

    Introduce ExclusionZones form page
    Add the new page for specifiying whether an applicant has any areas they
    should not be housed.

--- server/form-pages/apply/area-and-funding/area-information/index.ts
+++ server/form-pages/apply/area-and-funding/area-information/index.ts
@@ -3,11 +3,12 @@
 import { Task } from '../../../utils/decorators'
 import FirstPreferredArea from './firstPreferredArea'
 import SecondPreferredArea from './secondPreferredArea'
+import ExclusionZones from './exclusionZones'
 import FamilyAccommodation from './familyAccommodation'
 
 @Task({
   name: 'Add exclusion zones and preferred areas',
   slug: 'area-information',
-  pages: [FirstPreferredArea, SecondPreferredArea, FamilyAccommodation],
+  pages: [FirstPreferredArea, SecondPreferredArea, ExclusionZones, FamilyAccommodation],
 })
 export default class AreaInformation {}
