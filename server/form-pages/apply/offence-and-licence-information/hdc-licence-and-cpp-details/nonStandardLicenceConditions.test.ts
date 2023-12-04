d0e6e9694d56950690119901f1119a2a8d0dd83d
Patrick Fleming <patrick.fleming@dwx.com>
2023-12-04 10:36:11 +0000

    Add non-standard licence conditions page

---
+++ server/form-pages/apply/offence-and-licence-information/hdc-licence-and-cpp-details/nonStandardLicenceConditions.test.ts
@@ -0,0 +1,70 @@
+import { itShouldHavePreviousValue, itShouldHaveNextValue } from '../../../shared-examples'
+import { personFactory, applicationFactory } from '../../../../testutils/factories/index'
+import NonStandardLicenceConditions from './nonStandardLicenceConditions'
+
+describe('NonStandardLicenceConditions', () => {
+  const application = applicationFactory.build({ person: personFactory.build({ name: 'Roger Smith' }) })
+
+  describe('title', () => {
+    it('personalises the page title', () => {
+      const page = new NonStandardLicenceConditions({}, application)
+
+      expect(page.title).toEqual('Does Roger Smith have any non-standard licence conditions?')
+    })
+  })
+
+  itShouldHavePreviousValue(new NonStandardLicenceConditions({}, application), 'cpp-details')
+  itShouldHaveNextValue(new NonStandardLicenceConditions({}, application), '')
+
+  describe('items', () => {
+    it('returns the radio with the expected label text', () => {
+      const page = new NonStandardLicenceConditions(
+        {
+          nonStandardLicenceConditions: 'no',
+        },
+        application,
+      )
+
+      expect(page.items('some html')).toEqual([
+        {
+          value: 'yes',
+          text: 'Yes',
+          checked: false,
+          conditional: { html: 'some html' },
+        },
+        {
+          value: 'no',
+          text: 'No',
+          checked: true,
+        },
+        {
+          divider: 'or',
+        },
+        {
+          value: 'dontKnow',
+          text: `I don't know`,
+          checked: false,
+        },
+      ])
+    })
+  })
+
+  describe('errors', () => {
+    describe('when they have not provided any answer', () => {
+      it('returns an error', () => {
+        const page = new NonStandardLicenceConditions({}, application)
+        expect(page.errors()).toEqual({
+          nonStandardLicenceConditions: "Choose either Yes, No or I don't know",
+        })
+      })
+    })
+    describe('when they have not provided detail', () => {
+      it('returns an error', () => {
+        const page = new NonStandardLicenceConditions({ nonStandardLicenceConditions: 'yes' }, application)
+        expect(page.errors()).toEqual({
+          nonStandardLicenceConditionsDetail: 'Describe their non-standard licence conditions',
+        })
+      })
+    })
+  })
+})
---
+++ server/form-pages/apply/offence-and-licence-information/hdc-licence-and-cpp-details/nonStandardLicenceConditions.ts
@@ -0,0 +1,72 @@
+import { Radio, TaskListErrors, YesNoOrDontKnow } from '@approved-premises/ui'
+import { Cas2Application as Application } from '@approved-premises/api'
+import { Page } from '../../../utils/decorators'
+import TaskListPage from '../../../taskListPage'
+import { nameOrPlaceholderCopy } from '../../../../utils/utils'
+import { getQuestions } from '../../../utils/questions'
+import { convertKeyValuePairToRadioItems } from '../../../../utils/formUtils'
+
+type NonStandardLicenceConditionsBody = {
+  nonStandardLicenceConditions: YesNoOrDontKnow
+  nonStandardLicenceConditionsDetail: string
+}
+
+@Page({
+  name: 'non-standard-licence-conditions',
+  bodyProperties: ['nonStandardLicenceConditions', 'nonStandardLicenceConditionsDetail'],
+})
+export default class NonStandardLicenceConditions implements TaskListPage {
+  documentTitle = 'Does the person have any non-standard licence conditions?'
+
+  personName = nameOrPlaceholderCopy(this.application.person)
+
+  title
+
+  questions = getQuestions(this.personName)['hdc-licence-and-cpp-details']['non-standard-licence-conditions']
+
+  body: NonStandardLicenceConditionsBody
+
+  constructor(
+    body: Partial<NonStandardLicenceConditionsBody>,
+    private readonly application: Application,
+  ) {
+    this.body = body as NonStandardLicenceConditionsBody
+    this.title = this.questions.nonStandardLicenceConditions.question
+  }
+
+  previous() {
+    return 'cpp-details'
+  }
+
+  next() {
+    return ''
+  }
+
+  items(detailConditionalHtml: string) {
+    const items = convertKeyValuePairToRadioItems(
+      this.questions.nonStandardLicenceConditions.answers,
+      this.body.nonStandardLicenceConditions,
+    ) as Array<Radio>
+
+    items.forEach(item => {
+      if (item.value === 'yes') {
+        item.conditional = { html: detailConditionalHtml }
+      }
+    })
+
+    const dontKnow = items.pop()
+
+    return [...items, { divider: 'or' }, { ...dontKnow }]
+  }
+
+  errors() {
+    const errors: TaskListErrors<this> = {}
+    if (!this.body.nonStandardLicenceConditions) {
+      errors.nonStandardLicenceConditions = "Choose either Yes, No or I don't know"
+    }
+    if (this.body.nonStandardLicenceConditions === 'yes' && !this.body.nonStandardLicenceConditionsDetail) {
+      errors.nonStandardLicenceConditionsDetail = 'Describe their non-standard licence conditions'
+    }
+    return errors
+  }
+}
