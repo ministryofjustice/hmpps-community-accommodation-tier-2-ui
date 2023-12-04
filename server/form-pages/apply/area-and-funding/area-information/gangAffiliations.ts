6f56ad737ef9f3e45db7db68d6c7b08035452a59
Daniel Liburd <daniel.liburd@gmail.com>
2023-12-04 12:40:23 +0000

    Introduce GangAffiliations form page
    Add the new page for specifiying whether an applicant has any gang
    affiliations.

---
+++ server/form-pages/apply/area-and-funding/area-information/gangAffiliations.ts
@@ -0,0 +1,62 @@
+import { Cas2Application as Application } from '@approved-premises/api'
+import { TaskListErrors, YesOrNo } from '@approved-premises/ui'
+import { nameOrPlaceholderCopy } from '../../../../utils/utils'
+import { Page } from '../../../utils/decorators'
+import TaskListPage from '../../../taskListPage'
+import { getQuestions } from '../../../utils/questions'
+
+type GangAffiliationsBody = {
+  hasGangAffiliations: YesOrNo
+  gangName: string
+  gangOperationArea: string
+  rivalGangDetail: string
+}
+
+@Page({
+  name: 'gang-affiliations',
+  bodyProperties: ['hasGangAffiliations', 'gangName', 'gangOperationArea', 'rivalGangDetail'],
+})
+export default class GangAffiliations implements TaskListPage {
+  documentTitle = 'Does the person have any gang affiliations?'
+
+  personName = nameOrPlaceholderCopy(this.application.person)
+
+  title = `Does ${nameOrPlaceholderCopy(this.application.person)} have any gang affiliations?`
+
+  questions = getQuestions(this.personName)['area-information']['gang-affiliations']
+
+  body: GangAffiliationsBody
+
+  constructor(
+    body: Partial<GangAffiliationsBody>,
+    private readonly application: Application,
+  ) {
+    this.body = body as GangAffiliationsBody
+  }
+
+  previous() {
+    return 'exclusion-zones'
+  }
+
+  next() {
+    return 'family-accommodation'
+  }
+
+  errors() {
+    const errors: TaskListErrors<this> = {}
+
+    if (!this.body.hasGangAffiliations) {
+      errors.hasGangAffiliations = 'Select yes if they have gang affiliations'
+    }
+
+    if (this.body.hasGangAffiliations === 'yes' && !this.body.gangName) {
+      errors.gangName = `Enter the gang's name`
+    }
+
+    if (this.body.hasGangAffiliations === 'yes' && !this.body.gangOperationArea) {
+      errors.gangOperationArea = 'Describe the area the gang operates in'
+    }
+
+    return errors
+  }
+}
