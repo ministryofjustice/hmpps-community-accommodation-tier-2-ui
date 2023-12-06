1e3cc40a998b08835310877437d60062a057b5a4
Patrick Fleming <patrick.fleming@dwx.com>
2023-12-06 16:07:00 +0000

    Add consent task and extend before you start section definition

--- server/form-pages/apply/before-you-start/index.ts
+++ server/form-pages/apply/before-you-start/index.ts
@@ -2,9 +2,10 @@
 
 import { Section } from '../../utils/decorators'
 import ConfirmEligibility from './confirm-eligibility'
+import ConfirmConsent from './confirm-consent'
 
 @Section({
   title: 'Before you start',
-  tasks: [ConfirmEligibility],
+  tasks: [ConfirmEligibility, ConfirmConsent],
 })
 export default class BeforeYouStart {}
