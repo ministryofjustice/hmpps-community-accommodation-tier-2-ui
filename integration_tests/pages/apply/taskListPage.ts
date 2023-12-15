e8f25fd2b7a2b8a68ecbef6d04fba09eb26039eb
Harriet H-W <harriethw@gmail.com>
2023-12-15 16:53:08 +0000

    Add summary info to Task List

--- integration_tests/pages/apply/taskListPage.ts
+++ integration_tests/pages/apply/taskListPage.ts
@@ -6,7 +6,7 @@ import { FullPerson } from '../../../server/@types/shared/models/FullPerson'
 
 export default class TaskListPage extends Page {
   constructor(name: string) {
-    super('CAS 2: Refer for Accommodation', name)
+    super('Apply for Short-Term Accommodation (CAS-2)', name)
   }
 
   static visit(application: Cas2Application): TaskListPage {
