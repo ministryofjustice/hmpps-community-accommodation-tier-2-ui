6ddf8a485c79afe66b879b58c6aa93f8680de4ee
Harriet H-W <harriethw@gmail.com>
2023-12-19 15:30:14 +0000

    Rename E&D monitoring task

--- integration_tests/pages/apply/check_your_answers/check-your-answers/checkYourAnswersPage.ts
+++ integration_tests/pages/apply/check_your_answers/check-your-answers/checkYourAnswersPage.ts
@@ -22,7 +22,7 @@ export default class CheckYourAnswersPage extends ApplyPage {
   shouldShowEqualityAndDiversityAnswers(): void {
     this.shouldShowCheckYourAnswersTitle(
       'equality-and-diversity-monitoring',
-      'Add equality and diversity monitoring',
+      'Add equality and diversity monitoring information',
     )
     this.shouldShowQuestionsAndAnswers('equality-and-diversity-monitoring')
   }
