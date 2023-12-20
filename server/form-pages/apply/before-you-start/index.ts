9b0fbdff9c0850d6ebe8325baecc50d515038c16
Patrick Fleming <patrick.fleming@dwx.com>
2023-12-20 15:36:34 +0000

    Add ‘Check information needed’ task definition
    And extend ‘Before you apply’ section to include it.

--- server/form-pages/apply/before-you-start/index.ts
+++ server/form-pages/apply/before-you-start/index.ts
@@ -4,9 +4,10 @@ import { Section } from '../../utils/decorators'
 import ConfirmEligibility from './confirm-eligibility'
 import ConfirmConsent from './confirm-consent'
 import ReferrerDetails from './referrer-details'
+import CheckInformationNeeded from './information-needed-from-applicant'
 
 @Section({
   title: 'Before you apply',
-  tasks: [ConfirmEligibility, ConfirmConsent, ReferrerDetails],
+  tasks: [ConfirmEligibility, ConfirmConsent, ReferrerDetails, CheckInformationNeeded],
 })
 export default class BeforeYouStart {}
