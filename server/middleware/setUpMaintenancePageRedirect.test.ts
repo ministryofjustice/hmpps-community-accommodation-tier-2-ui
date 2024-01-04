cb7413a3ab6bf7ba6b04088b9ef66819135c19d8
Tom Hipkin <tomhipkin@gmail.com>
2024-01-04 18:07:34 +0000

    Use the StaticController pattern to add a maintenance page
    The pattern can be found in here in CAS3[1].
    
    [1] https://github.com/ministryofjustice/hmpps-temporary-accommodation-ui/pull/699/files

--- server/middleware/setUpMaintenancePageRedirect.test.ts
+++ server/middleware/setUpMaintenancePageRedirect.test.ts
@@ -7,12 +7,17 @@ const setupApp = (): Express => {
   const app = express()
   app.use(setUpMaintenancePageRedirect())
 
-  app.get('/known', (_req, res, _next) => {
-    res.send('known')
+  const allowedPaths = ['/known', '/maintenance', '/health', '/sign-in', '/sign-in/callback']
+
+  allowedPaths.forEach(path => {
+    app.get(path, (_req, res, _next) => {
+      res.send(path.slice(1))
+    })
   })
 
   return app
 }
+
 describe('setUpMaintenancePageRedirect', () => {
   describe('when the MAINTENANCE_MODE environment is NOT set', () => {
     it('should not redirect to the maintenance page', () => {
@@ -36,6 +41,32 @@ describe('setUpMaintenancePageRedirect', () => {
       const app = setupApp()
       const response = await request(app).get('/known').expect(302)
       expect(response.text).toContain('Found. Redirecting to /maintenance')
+    })
+
+    describe('and the requested page should not be redirected', () => {
+      it('should not redirect requests for the maintenance page, endlessly back to the maintenance page', () => {
+        config.flags.maintenanceMode = 'true'
+        const app = setupApp()
+        return request(app).get('/maintenance').expect(200)
+      })
+
+      it('should not redirect health requests to the maintenance page', () => {
+        config.flags.maintenanceMode = 'true'
+        const app = setupApp()
+        return request(app).get('/health').expect(200)
+      })
+
+      it('should not redirect sign in requests to the maintenance page', () => {
+        config.flags.maintenanceMode = 'true'
+        const app = setupApp()
+        return request(app).get('/sign-in').expect(200)
+      })
+
+      it('should not redirect sign in callbacks to the maintenance page', () => {
+        config.flags.maintenanceMode = 'true'
+        const app = setupApp()
+        return request(app).get('/sign-in/callback').expect(200)
+      })
     })
   })
 })
--- server/middleware/setUpMaintenancePageRedirect.ts
+++ server/middleware/setUpMaintenancePageRedirect.ts
@@ -3,9 +3,10 @@ import config from '../config'
 
 export default function setUpMaintenancePageRedirect(): Router {
   const router = express.Router()
+  const allowedPaths = ['/sign-in', '/sign-in/callback', '/health', '/maintenance']
 
-  router.use((_req, res, next) => {
-    if (config.flags.maintenanceMode === 'true') {
+  router.use((req, res, next) => {
+    if (config.flags.maintenanceMode === 'true' && !allowedPaths.includes(req.path)) {
       return res.redirect(302, `/maintenance`)
     }
     return next()
