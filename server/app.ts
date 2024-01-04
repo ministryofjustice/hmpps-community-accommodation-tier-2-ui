1b112c51346cb5f4ca58efcf3c4828c43f43984a
Tom Hipkin <tomhipkin@gmail.com>
2024-01-04 15:30:21 +0000

    Sort imports alphabetically
    I have my editor set up to do this automatically to make things easier to read:
    
    ```
     "editor.codeActionsOnSave": {
        "source.sortImports": true
    ```
    
    We can either not do this and I can turn this off or we can start to sort alphabetically if it’s valuable.

--- server/app.ts
+++ server/app.ts
@@ -1,30 +1,30 @@
 /* istanbul ignore file */
 
-import path from 'path'
-import express from 'express'
 import flash from 'connect-flash'
+import express from 'express'
+import path from 'path'
 
 import createError from 'http-errors'
 import methodOverride from 'method-override'
 
-import nunjucksSetup from './utils/nunjucksSetup'
 import errorHandler from './errorHandler'
 import authorisationMiddleware from './middleware/authorisationMiddleware'
 import { metricsMiddleware } from './monitoring/metricsApp'
+import nunjucksSetup from './utils/nunjucksSetup'
 
 import setUpAuthentication from './middleware/setUpAuthentication'
 import setUpCsrf from './middleware/setUpCsrf'
 import setUpCurrentUser from './middleware/setUpCurrentUser'
 import setUpHealthChecks from './middleware/setUpHealthChecks'
+import { setUpSentryErrorHandler, setUpSentryRequestHandler } from './middleware/setUpSentry'
 import setUpStaticResources from './middleware/setUpStaticResources'
-import setUpWebRequestParsing from './middleware/setupRequestParsing'
 import setUpWebSecurity from './middleware/setUpWebSecurity'
 import setUpWebSession from './middleware/setUpWebSession'
-import { setUpSentryErrorHandler, setUpSentryRequestHandler } from './middleware/setUpSentry'
+import setUpWebRequestParsing from './middleware/setupRequestParsing'
 
+import { Controllers } from './controllers'
 import routes from './routes'
 import type { Services } from './services'
-import { Controllers } from './controllers'
 
 export default function createApp(controllers: Controllers, services: Services): express.Application {
   const app = express()
