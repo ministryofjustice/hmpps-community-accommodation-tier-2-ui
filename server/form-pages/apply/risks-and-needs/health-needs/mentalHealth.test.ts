9476418816b07c797f5bbad06a0a153adf308872
Patrick Fleming <patrick.fleming@dxw.com>
2024-01-18 15:30:28 +0000

    Remove mental health page questions tests
    These tests are now redundant thanks to our questions util function.

--- server/form-pages/apply/risks-and-needs/health-needs/mentalHealth.test.ts
+++ server/form-pages/apply/risks-and-needs/health-needs/mentalHealth.test.ts
@@ -13,39 +13,6 @@ describe('MentalHealth', () => {
     })
   })
 
-  describe('Questions', () => {
-    const page = new MentalHealth({}, application)
-
-    describe('hasMentalHealthNeeds', () => {
-      it('has a question', () => {
-        expect(page.questions.hasMentalHealthNeeds.question).toBeDefined()
-      })
-      it('has one follow-up question', () => {
-        expect(page.questions.needsDetail.question).toBeDefined()
-      })
-    })
-
-    describe('isEngagedWithCommunity', () => {
-      it('has a question', () => {
-        expect(page.questions.isEngagedWithCommunity.question).toBeDefined()
-      })
-      it('has one follow-up question', () => {
-        expect(page.questions.servicesDetail.question).toBeDefined()
-      })
-    })
-
-    describe('hasPrescribedMedication', () => {
-      it('has a question', () => {
-        expect(page.questions.hasPrescribedMedication.question).toBeDefined()
-      })
-      it('has 3 follow-up questions', () => {
-        expect(page.questions.isInPossessionOfMeds.question).toBeDefined()
-        expect(page.questions.medicationDetail.question).toBeDefined()
-        expect(page.questions.medicationIssues.question).toBeDefined()
-      })
-    })
-  })
-
   itShouldHaveNextValue(new MentalHealth({}, application), 'communication-and-language')
   itShouldHavePreviousValue(new MentalHealth({}, application), 'physical-health')
 
