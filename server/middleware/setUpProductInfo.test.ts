f8f50b54f86daa1967a68a813d8ea12e3ae591ed
Harriet H-W <harriethw@gmail.com>
2024-01-29 10:59:49 +0000

    add private beta prison roll-out to product info
    this `/info` endpoint was added in a previous PR
    https://github.com/ministryofjustice/hmpps-community-accommodation-tier-2-ui/pull/479
    
    This adds the prison codes for the prisons we are releasing to in
    private beta.
    
    Once it's released, an hourly job on DPS side should check it to determine
    what prisons will be able to see the service on the DPS home page [1]
    
    [1]
    https://dsdmoj.atlassian.net/wiki/spaces/ALIGN/pages/4480959439/Adding+changing+services+on+the+common+header+footer+and+the+homepage#Establishment-specific-rollouts

--- server/middleware/setUpProductInfo.test.ts
+++ server/middleware/setUpProductInfo.test.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express'
 import request from 'supertest'
 
 import setUpProductInfo from './setUpProductInfo'
+import activePrisons from '../utils/activePrisons'
 
 const setupApp = (): Express => {
   const app = express()
@@ -10,13 +11,13 @@ const setupApp = (): Express => {
 }
 
 describe('setUpProductInfo', () => {
-  it('should show an empty list of activeAgencies', async () => {
+  it('should show a list of our active Private Beta prisons as activeAgencies', async () => {
     const app = setupApp()
     request(app)
       .get('/info')
       .expect(200)
       .then(response => {
-        expect(response.body.activeAgencies).toEqual([])
+        expect(response.body.activeAgencies).toEqual(activePrisons)
       })
   })
 })
--- server/middleware/setUpProductInfo.ts
+++ server/middleware/setUpProductInfo.ts
@@ -1,11 +1,12 @@
 import express, { Router } from 'express'
+import activePrisons from '../utils/activePrisons'
 
 export default function setUpProductInfo(): Router {
   const router = express.Router()
 
   router.get('/info', (req, res) => {
     res.json({
-      activeAgencies: [],
+      activeAgencies: activePrisons,
     })
   })
 
