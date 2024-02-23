af03c1e7172549d535ebd59e575a11911def319b
Patrick Fleming <patrick.fleming@dxw.com>
2024-02-23 11:16:59 +0000

    Update E2E tests for add a note feature (assessor)

--- e2e-tests/tests/03_assess.spec.ts
+++ e2e-tests/tests/03_assess.spec.ts
@@ -1,5 +1,5 @@
 import { Page, expect } from '@playwright/test'
-import { signInAsAssessor, updateStatus, viewSubmittedApplication } from '../steps/assess'
+import { signInAsAssessor, updateStatus, viewSubmittedApplication, addNote } from '../steps/assess'
 import { test } from '../test'
 
 test('view a submitted application as an assessor', async ({ page, assessorUser }) => {
@@ -8,6 +8,7 @@ test('view a submitted application as an assessor', async ({ page, assessorUser 
   await viewSubmittedApplication(page)
   await updateStatus(page)
   await expect(page.locator('.moj-timeline__title').first()).toContainText('More information requested')
+  await addNote(page)
 })
 
 const signOut = async (page: Page) => {
