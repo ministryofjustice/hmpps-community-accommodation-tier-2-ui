7d596b54599ee84c4baee084faf73b8668c5a29b
Patrick Fleming <patrick.fleming@dxw.com>
2024-04-08 15:12:04 +0100

    Update offence history integration tests

--- integration_tests/pages/apply/offence_and_licence_information/offending-history/offenceHistoryDataPage.ts
+++ integration_tests/pages/apply/offence_and_licence_information/offending-history/offenceHistoryDataPage.ts
@@ -26,13 +26,13 @@ export default class OffenceHistoryDataPage extends ApplyPage {
   addOffenceInformation(): void {
     this.getTextInputByIdAndEnterDetails('offenceGroupName', 'Arson (01000)')
     this.getSelectInputByIdAndSelectAnEntry('offenceCategory', 'Arson')
-    this.completeDateInputs('offenceDate', '2022-07-15')
-    this.getTextInputByIdAndEnterDetails('sentenceLength', '6 months')
+    this.getTextInputByIdAndEnterDetails('numberOfOffences', '2')
+    this.getTextInputByIdAndEnterDetails('sentenceTypes', '1 suspended')
     this.getTextInputByIdAndEnterDetails('summary', 'some detail')
   }
 
   clickSubmit(): void {
-    cy.get('button').contains('Save and continue').click()
+    cy.get('button').contains('Save and add offence').click()
   }
 
   clickAddAnother(): void {
