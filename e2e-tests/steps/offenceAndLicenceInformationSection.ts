121b53e95bfc36b4f892b03a4af0809cf6cd8e3c
Patrick Fleming <patrick.fleming@dxw.com>
2024-04-12 09:08:37 +0100

    Change ‘offence category’ text to ‘offence type’
    The discrepancy was causing E2E test failures, so I have corrected it in accordance with the up to date designs.

--- e2e-tests/steps/offenceAndLicenceInformationSection.ts
+++ e2e-tests/steps/offenceAndLicenceInformationSection.ts
@@ -12,7 +12,7 @@ export const completeCurrentOffencesTask = async (page: Page, name: string) => {
 async function completeCurrentOffenceDetailsPage(page: Page, name: string) {
   const currentOffenceDetailsPage = await ApplyPage.initialize(page, `Add ${name}'s current offence details`)
   await currentOffenceDetailsPage.fillField('Offence title', 'Stalking')
-  await currentOffenceDetailsPage.chooseSelectItem('Offence category', 'Stalking or Harassment')
+  await currentOffenceDetailsPage.chooseSelectItem('Offence type', 'Stalking or Harassment')
   await currentOffenceDetailsPage.fillDateFieldInGroup('When did they commit the offence?', {
     year: '2022',
     month: '3',
