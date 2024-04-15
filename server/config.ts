4ef43574d98439d7c9ed5f6fdbe4e9bd36415b6f
Harriet H-W <harriethw@gmail.com>
2024-04-15 16:55:22 +0100

    set prison dashboard feature flag to be based on environment
    We discovered that env variables are types of strings rather than
    booleans - that meant that if the env variable was set to `false`, in javascript
    land that was becoming a string `'false'` and always evaluating as true.
    
    This removes the flag as an environment variable and just sets the flag
    based on what the environment is - this should still allow us to easily turn
    it off/on and remove it once we're done.

--- server/config.ts
+++ server/config.ts
@@ -101,7 +101,7 @@ export default {
   domain: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
   flags: {
     maintenanceMode: get('IN_MAINTENANCE_MODE', false),
-    isPrisonDashboardEnabled: get('PRISON_DASHBOARD_ENABLED', false),
+    isPrisonDashboardEnabled: ['local', 'dev', 'test'].includes(process.env.ENVIRONMENT || 'local'),
   },
   analytics: {
     tagManagerId: get('TAG_MANAGER_ID', null),
