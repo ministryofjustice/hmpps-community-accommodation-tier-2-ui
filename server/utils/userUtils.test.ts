ministryofjustice/hmpps-supported-accommodation-ui

show prison dashboard tile for POM users

server/utils/userUtils.test.ts
@@ -1,3 +1,4 @@
+import config from '../config'
 import { sectionsForUser, sections } from './userUtils'
 
 describe('userUtils', () => {
@@ -6,9 +7,20 @@ describe('userUtils', () => {
       expect(sectionsForUser([])).toEqual([])
     })
 
-    it('should return correct sections for a POM', () => {
-      const expected = [sections.referral, sections.newReferral]
-      expect(sectionsForUser(['ROLE_POM'])).toEqual(expected)
+    describe('when prison dashboard is enabled', () => {
+      it('should return correct sections including the prison dashboard for a POM', () => {
+        config.flags.prisonDashboardEnabled = true
+        const expected = [sections.referral, sections.newReferral, sections.prisonDashboard]
+        expect(sectionsForUser(['ROLE_POM'])).toEqual(expected)
+      })
+    })
+
+    describe('when prison dashboard is NOT enabled', () => {
+      it('should return correct sections without prison dashboard for a POM', () => {
+        config.flags.prisonDashboardEnabled = false
+        const expected = [sections.referral, sections.newReferral]
+        expect(sectionsForUser(['ROLE_POM'])).toEqual(expected)
+      })
     })
 
     it('should return correct sections for an admin', () => {

server/utils/userUtils.ts
@@ -3,6 +3,7 @@ import { ServiceSection } from '@approved-premises/ui'
 import applyPaths from '../paths/apply'
 import assessPaths from '../paths/assess'
 import reportsPaths from '../paths/report'
+import config from '../config'
 
 export const sections = {
   referral: {
@@ -33,6 +34,13 @@ export const sections = {
     shortTitle: 'Management information reports',
     href: reportsPaths.report.new.pattern,
   },
+  prisonDashboard: {
+    id: 'prison-dashboard',
+    title: 'View my prison’s referrals',
+    description: 'View recently submitted CAS-2 referrals from your prison.',
+    shortTitle: 'View my prison’s referrals',
+    href: reportsPaths.report.new.pattern,
+  },
 }
 
 export const hasRole = (userRoles: Array<string>, role: string): boolean => {
@@ -45,6 +53,9 @@ export const sectionsForUser = (userRoles: Array<string>): Array<ServiceSection>
   if (hasRole(userRoles, 'ROLE_POM')) {
     items.push(sections.referral)
     items.push(sections.newReferral)
+    if (config.flags.prisonDashboardEnabled) {
+      items.push(sections.prisonDashboard)
+    }
   }
   if (hasRole(userRoles, 'ROLE_CAS2_ADMIN')) {
     items.push(sections.submittedApplications)

ministryofjustice/hmpps-community-accommodation-tier-2-ui

server/utils/userUtils.test.ts
@@ -1,3 +1,4 @@
+import config from '../config'
 import { sectionsForUser, sections } from './userUtils'
 
 describe('userUtils', () => {
@@ -6,9 +7,20 @@ describe('userUtils', () => {
       expect(sectionsForUser([])).toEqual([])
     })
 
-    it('should return correct sections for a POM', () => {
-      const expected = [sections.referral, sections.newReferral]
-      expect(sectionsForUser(['ROLE_POM'])).toEqual(expected)
+    describe('when prison dashboard is enabled', () => {
+      it('should return correct sections including the prison dashboard for a POM', () => {
+        config.flags.isPrisonDashboardEnabled = true
+        const expected = [sections.referral, sections.newReferral, sections.prisonDashboard]
+        expect(sectionsForUser(['ROLE_POM'])).toEqual(expected)
+      })
+    })
+
+    describe('when prison dashboard is NOT enabled', () => {
+      it('should return correct sections without prison dashboard for a POM', () => {
+        config.flags.isPrisonDashboardEnabled = false
+        const expected = [sections.referral, sections.newReferral]
+        expect(sectionsForUser(['ROLE_POM'])).toEqual(expected)
+      })
     })
 
     it('should return correct sections for an admin', () => {

server/utils/userUtils.ts
@@ -3,6 +3,7 @@ import { ServiceSection } from '@approved-premises/ui'
 import applyPaths from '../paths/apply'
 import assessPaths from '../paths/assess'
 import reportsPaths from '../paths/report'
+import config from '../config'
 
 export const sections = {
   referral: {
@@ -33,6 +34,13 @@ export const sections = {
     shortTitle: 'Management information reports',
     href: reportsPaths.report.new.pattern,
   },
+  prisonDashboard: {
+    id: 'prison-dashboard',
+    title: 'View your prison’s applications',
+    description: 'View recently submitted CAS-2 applications from your prison.',
+    shortTitle: 'View your prison’s applications',
+    href: applyPaths.applications.prison.pattern,
+  },
 }
 
 export const hasRole = (userRoles: Array<string>, role: string): boolean => {
@@ -45,6 +53,9 @@ export const sectionsForUser = (userRoles: Array<string>): Array<ServiceSection>
   if (hasRole(userRoles, 'ROLE_POM')) {
     items.push(sections.referral)
     items.push(sections.newReferral)
+    if (config.flags.isPrisonDashboardEnabled) {
+      items.push(sections.prisonDashboard)
+    }
   }
   if (hasRole(userRoles, 'ROLE_CAS2_ADMIN')) {
     items.push(sections.submittedApplications)