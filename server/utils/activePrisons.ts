95c9a0058e104e63f17541cbe7108b4b58794052
Tom Hipkin <tomhipkin@gmail.com>
2024-04-25 12:17:38 +0100

    All prisons may see and navigate to CAS2 from their DPS landing page
    Configured based on this guidance[1]
    
    [1] https://dsdmoj.atlassian.net/wiki/spaces/NDSS/pages/4616488213/Publishing+the+deployment+scope+of+a+product

--- server/utils/activePrisons.ts
+++ server/utils/activePrisons.ts
@@ -1,19 +1,4 @@
-export default [
-  'ACI',
-  'BWI',
-  'BZI',
-  'CWI',
-  'DHI',
-  'EWI',
-  'FWI',
-  'FEI',
-  'HHI',
-  'HMI',
-  'LFI',
-  'LNI',
-  'OWI',
-  'ONI',
-  'PRI',
-  'RNI',
-  'WEI',
-]
+// All prisons are marked as active. All users from an active prison can see and
+// navigate to CAS2 on their DPS landing page.
+// https://dsdmoj.atlassian.net/wiki/spaces/NDSS/pages/4616488213/Publishing+the+deployment+scope+of+a+product
+export default ['***']
