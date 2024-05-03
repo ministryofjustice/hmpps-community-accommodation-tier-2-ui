55dd95353c05d8961b57bf4c3a19ded756b66c25
Patrick Fleming <patrick.fleming@dxw.com>
2024-05-03 15:03:31 +0100

    Add new Old OASys page to risk to self task

--- server/form-pages/apply/risks-and-needs/risk-to-self/index.ts
+++ server/form-pages/apply/risks-and-needs/risk-to-self/index.ts
@@ -8,10 +8,11 @@ import CurrentRisk from './currentRisk'
 import HistoricalRisk from './historicalRisk'
 import OasysImport from './custom-forms/oasysImport'
 import Vulnerability from './vulnerability'
+import OldOasys from './oldOasys'
 
 @Task({
   name: 'Add risk to self information',
   slug: 'risk-to-self',
-  pages: [OasysImport, Vulnerability, CurrentRisk, HistoricalRisk, AcctData, Acct, AdditionalInformation],
+  pages: [OasysImport, OldOasys, Vulnerability, CurrentRisk, HistoricalRisk, AcctData, Acct, AdditionalInformation],
 })
 export default class RiskToSelf {}
