c0c090087c0a92e3685d4558970d22326fd74027
Harriet H-W <harriethw@gmail.com>
2024-05-07 11:53:33 +0100

    add status updates to an Assessment in test factories

--- server/testutils/factories/assessment.ts
+++ server/testutils/factories/assessment.ts
@@ -1,9 +1,11 @@
 import { faker } from '@faker-js/faker/locale/en_GB'
 import { Factory } from 'fishery'
 import { Cas2Assessment } from '@approved-premises/api'
+import statusUpdateFactory from './statusUpdate'
 
 export default Factory.define<Cas2Assessment>(() => ({
   id: faker.string.uuid(),
   nacroReferralId: faker.string.uuid(),
   assessorName: faker.person.fullName(),
+  statusUpdates: statusUpdateFactory.buildList(2),
 }))
