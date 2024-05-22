71f603e49eb6b9fa40b8b8345aec130e9ee4737b
Patrick Fleming <patrick.fleming@dxw.com>
2024-05-22 09:21:58 +0100

    Remove back to task list link from ACCT page and update additional information styles
    We remove the ‘Back to task list’ link from the ACCT page, to reduce the chances of a user failing to click ‘Save and continue’, leaving the task incomplete.
    
    We also update the additional information page heading and stylings.

--- integration_tests/pages/apply/risks_and_needs/risk-to-self/additionalInformationPage.ts
+++ integration_tests/pages/apply/risks_and_needs/risk-to-self/additionalInformationPage.ts
@@ -1,10 +1,16 @@
 import { Cas2Application as Application } from '../../../../../server/@types/shared/models/Cas2Application'
 import ApplyPage from '../../applyPage'
 import paths from '../../../../../server/paths/apply'
+import { nameOrPlaceholderCopy } from '../../../../../server/utils/utils'
 
 export default class AdditionalInformationPage extends ApplyPage {
   constructor(private readonly application: Application) {
-    super('Additional Information', application, 'risk-to-self', 'additional-information')
+    super(
+      `Is there anything else to include about ${nameOrPlaceholderCopy(application.person)}'s risk to self?`,
+      application,
+      'risk-to-self',
+      'additional-information',
+    )
   }
 
   static visit(application: Application): void {
