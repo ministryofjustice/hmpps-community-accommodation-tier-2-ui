222818191b229927290c9c3435caf9e0a809bb6f
Brian McHugh <brian.mchugh@digital.justice.gov.uk>
2024-12-12 10:03:09 +0000

    Remove JQuery and update moj frontend to latest version

---
+++ assets/js/mojFrontEndInit.js
@@ -0,0 +1,4 @@
+const $buttonmenu = document.querySelectorAll('[data-module="moj-button-menu"]')
+MOJFrontend.nodeListForEach($buttonmenu, function ($buttonmenu) {
+  new MOJFrontend.ButtonMenu($buttonmenu, {}).init();
+})
