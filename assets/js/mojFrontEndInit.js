072df7b0ae69fdd64603817d765b2bf84b03b7c4
Brian McHugh <brian.mchugh@digital.justice.gov.uk>
2024-12-16 11:37:52 +0000

    Add debugging to init of moj components

--- assets/js/mojFrontEndInit.js
+++ assets/js/mojFrontEndInit.js
@@ -1,4 +1,14 @@
-const $buttonmenu = document.querySelectorAll('[data-module="moj-button-menu"]')
-MOJFrontend.nodeListForEach($buttonmenu, function ($buttonmenu) {
+const $buttonmenu = document.querySelectorAll('[data-module="moj-button-menu"]');
+
+const debugBeforeLoop = document.createElement('div');
+debugBeforeLoop.textContent = 'Debug: Starting to process button menus...';
+debugBeforeLoop.style.color = 'red';
+document.body.prepend(debugBeforeLoop);
+
+MOJFrontend.nodeListForEach($buttonmenu, function ($buttonmenu, index) {
+  const debugInLoop = document.createElement('div');
+  debugInLoop.textContent = `Debug: Processing button menu #${index + 1}`;
+  debugInLoop.style.color = 'blue';
+  document.body.prepend(debugInLoop);
   new MOJFrontend.ButtonMenu($buttonmenu, {}).init();
-})
+});
