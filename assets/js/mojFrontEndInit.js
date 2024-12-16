99c3cb5fcb444312f008015defc3b5dbb025afa3
Brian McHugh <brian.mchugh@digital.justice.gov.uk>
2024-12-16 15:40:40 +0000

    Update reference to JS

--- assets/js/mojFrontEndInit.js
+++ assets/js/mojFrontEndInit.js
@@ -1,14 +1,4 @@
 const $buttonmenu = document.querySelectorAll('[data-module="moj-button-menu"]');
-
-const debugBeforeLoop = document.createElement('div');
-debugBeforeLoop.textContent = 'Debug: Starting to process button menus...';
-debugBeforeLoop.style.color = 'red';
-document.body.prepend(debugBeforeLoop);
-
-MOJFrontend.nodeListForEach($buttonmenu, function ($buttonmenu, index) {
-  const debugInLoop = document.createElement('div');
-  debugInLoop.textContent = `Debug: Processing button menu #${index + 1}`;
-  debugInLoop.style.color = 'blue';
-  document.body.prepend(debugInLoop);
+MOJFrontend.nodeListForEach($buttonmenu, function ($buttonmenu) {
   new MOJFrontend.ButtonMenu($buttonmenu, {}).init();
 });
