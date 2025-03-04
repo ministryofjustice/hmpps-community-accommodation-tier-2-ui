030561faaeee72a693ccad5a9dde7d33bb9ef0d2
Alistair Laing <aliuk2012@users.noreply.github.com>
2025-03-04 11:59:56 +0000

    Add App insights to asset pipeline

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -19,6 +19,7 @@ export default function setUpStaticResources(): Router {
     '/assets/js',
     '/node_modules/govuk-frontend/dist',
     '/node_modules/govuk-frontend/dist/govuk/assets',
+    '/node_modules/@microsoft',
     '/node_modules/@ministryofjustice/frontend/moj/assets',
     '/node_modules/@ministryofjustice/frontend',
   ).forEach(dir => {
