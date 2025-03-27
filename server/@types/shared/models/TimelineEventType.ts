8cdc6edafd5cc1c05d108893e7b2e5b818f53ecd
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-03-27 15:14:04 +0000

    Regenerate types for new timeline events

--- server/@types/shared/models/TimelineEventType.ts
+++ server/@types/shared/models/TimelineEventType.ts
@@ -2,4 +2,4 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export type TimelineEventType = 'cas3_person_arrived' | 'cas3_person_departed' | 'application_timeline_note' | 'cas2_application_submitted' | 'cas2_note' | 'cas2_status_update';
+export type TimelineEventType = 'cas3_person_arrived' | 'cas3_person_departed' | 'application_timeline_note' | 'cas2_application_submitted' | 'cas2_note' | 'cas2_status_update' | 'cas2_prison_transfer' | 'cas2_new_pom_assigned';
