a7f128131d0434eeb14b8d62bdea173592bcbcd2
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-03-28 14:25:30 +0000

    Randomise  timeline event types in test factory

--- server/testutils/factories/timelineEvents.ts
+++ server/testutils/factories/timelineEvents.ts
@@ -1,10 +1,18 @@
 import { faker } from '@faker-js/faker/locale/en_GB'
 import { Factory } from 'fishery'
-import { Cas2TimelineEvent } from '@approved-premises/api'
+import { Cas2TimelineEvent, TimelineEventType } from '@approved-premises/api'
 import { DateFormats } from '../../utils/dateUtils'
 
+const timelineEventTypes: TimelineEventType[] = [
+  'cas2_application_submitted',
+  'cas2_note',
+  'cas2_status_update',
+  'cas2_prison_transfer',
+  'cas2_new_pom_assigned',
+]
+
 export default Factory.define<Cas2TimelineEvent>(() => ({
-  type: 'cas2_status_update',
+  type: faker.helpers.arrayElement(timelineEventTypes),
   occurredAt: DateFormats.dateObjToIsoDateTime(faker.date.past()),
   label: 'More information requested',
   createdByName: 'CAS2 Assessor (NACRO)',
