b8e94572d2a27187ac30927e929918f3dcaba0d7
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-03-28 15:01:53 +0000

    Fix lint issues

--- server/testutils/factories/timelineEvents.ts
+++ server/testutils/factories/timelineEvents.ts
@@ -1,10 +1,10 @@
 import { faker } from '@faker-js/faker/locale/en_GB'
 import { Factory } from 'fishery'
-import { Cas2TimelineEvent, TimelineEventType } from '@approved-premises/api'
+import { Cas2TimelineEvent } from '@approved-premises/api'
 import { DateFormats } from '../../utils/dateUtils'
 
 export default Factory.define<Cas2TimelineEvent>(() => ({
-  type: "cas2_status_update",
+  type: 'cas2_status_update',
   occurredAt: DateFormats.dateObjToIsoDateTime(faker.date.past()),
   label: 'More information requested',
   createdByName: 'CAS2 Assessor (NACRO)',
