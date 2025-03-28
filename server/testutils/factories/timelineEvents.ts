910229c1de931df8eff9c80acd74276d29c8f3b8
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-03-28 14:57:51 +0000

    Remove random type on factory

--- server/testutils/factories/timelineEvents.ts
+++ server/testutils/factories/timelineEvents.ts
@@ -3,16 +3,8 @@ import { Factory } from 'fishery'
 import { Cas2TimelineEvent, TimelineEventType } from '@approved-premises/api'
 import { DateFormats } from '../../utils/dateUtils'
 
-const timelineEventTypes: TimelineEventType[] = [
-  'cas2_application_submitted',
-  'cas2_note',
-  'cas2_status_update',
-  'cas2_prison_transfer',
-  'cas2_new_pom_assigned',
-]
-
 export default Factory.define<Cas2TimelineEvent>(() => ({
-  type: faker.helpers.arrayElement(timelineEventTypes),
+  type: "cas2_status_update",
   occurredAt: DateFormats.dateObjToIsoDateTime(faker.date.past()),
   label: 'More information requested',
   createdByName: 'CAS2 Assessor (NACRO)',
