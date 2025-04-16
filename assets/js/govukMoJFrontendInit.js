49aaf696aec52b932c93e260223e6480710bd615
Alistair Laing <aliuk2012@users.noreply.github.com>
2025-04-16 08:18:55 +0100

    Update how we import js/css for govuk/moj frontend

---
+++ assets/js/govukMoJFrontendInit.js
@@ -0,0 +1,5 @@
+import * as MOJFrontend from '/assets/moj/moj-frontend.min.js'
+import * as GOVUKFrontend from './govuk/govuk-frontend.min.js'
+
+GOVUKFrontend.initAll()
+MOJFrontend.initAll()
--- assets/js/mojFrontEndInit.js
+++
@@ -1,4 +0,0 @@
-const $buttonmenu = document.querySelectorAll('[data-module="moj-button-menu"]');
-MOJFrontend.nodeListForEach($buttonmenu, function ($buttonmenu) {
-  new MOJFrontend.ButtonMenu($buttonmenu, {}).init();
-});
