e16e4f87571014397f1e3f0526f28a8fd9793fd8
Rhys St Romaine <rhys.stromaine@justice.gov.uk>
2025-08-12 14:43:44 +0100

    Update phasebanner

--- server/utils/phaseBannerUtils.test.ts
+++ server/utils/phaseBannerUtils.test.ts
@@ -2,7 +2,7 @@ import { getContent } from './phaseBannerUtils'
 
 describe('PhaseBannerUtils', () => {
   it('returns the phase banner content', () => {
-    const content = `This is a new service. <a class="govuk-link" rel="noreferrer noopener" target="_blank" href="https://forms.office.com/Pages/ResponsePage.aspx?id=KEeHxuZx_kGp4S6MNndq2KxXUZ1jbxlMsEoiPoZPWGNURVpKVERMMzRYOEpYS1cwVVBDUTZQUThFSS4u">Complete our feedback form</a> (opens in new tab) or <a class="govuk-link" href="mailto:cas2support@digital.justice.gov.uk">email us</a> (cas2support@digital.justice.gov.uk) to report a bug`
+    const content = `This is a new service. <a class="govuk-link" rel="noreferrer noopener" target="_blank" href="https://forms.office.com/Pages/ResponsePage.aspx?id=KEeHxuZx_kGp4S6MNndq2KxXUZ1jbxlMsEoiPoZPWGNURVpKVERMMzRYOEpYS1cwVVBDUTZQUThFSS4u">Complete our feedback form</a> (opens in new tab). To get help or report a bug, contact the Service Desk (open <a class="govuk-link" href="https://mojprod.service-now.com/moj_sp?id=sc_cat_item&table=sc_cat_item&sys_id=5a2f4eff1bbf1690a1e2ddf0b24bcb34">a support ticket</a> or call 0800 917 5148 or #6598 from inside an establishment).`
 
     expect(getContent()).toEqual(content)
   })
