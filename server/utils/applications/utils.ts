60414314d860168159a8b912be38b74df7d7fdd1
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-10-29 09:48:15 +0000

    Improve coverage
    Removes unused methods, adds some missing tests.

--- server/utils/applications/utils.ts
+++ server/utils/applications/utils.ts
@@ -1,11 +1,6 @@
 import { Request, Response } from 'express'
 import type { ApplicationDocument, FormPages, JourneyType, SideNavItem, UiTimelineEvent } from '@approved-premises/ui'
-import type {
-  Cas2Application as Application,
-  Cas2Application,
-  Cas2SubmittedApplication,
-  Cas2TimelineEvent,
-} from '@approved-premises/api'
+import type { Cas2Application as Application, Cas2Application, Cas2SubmittedApplication } from '@approved-premises/api'
 import { getSections } from '../checkYourAnswersUtils'
 import { stringToKebabCase, formatCommaToLinebreak } from '../utils'
 import { formatLines } from '../viewUtils'
@@ -55,32 +50,6 @@ export const hdcDatesHaveBeenEntered = (application: Application): boolean => {
 
 const consentAnswer = (application: Application): string => {
   return application.data?.['confirm-consent']?.['confirm-consent']?.hasGivenConsent
-}
-
-export const getTimelineEvents = (timelineEvents: Array<Cas2TimelineEvent>): Array<UiTimelineEvent> => {
-  if (timelineEvents) {
-    return timelineEvents
-      .sort((a, b) => Number(DateFormats.isoToDateObj(b.occurredAt)) - Number(DateFormats.isoToDateObj(a.occurredAt)))
-      .map(sortedTimelineEvents => {
-        const description =
-          sortedTimelineEvents.type === 'cas2_status_update' && sortedTimelineEvents.body
-            ? formatCommaToLinebreak(sortedTimelineEvents.body)
-            : formatLines(sortedTimelineEvents.body)
-
-        return {
-          label: { text: sortedTimelineEvents.label },
-          byline: {
-            text: sortedTimelineEvents.createdByName,
-          },
-          datetime: {
-            timestamp: sortedTimelineEvents.occurredAt,
-            type: 'datetime',
-          },
-          html: description,
-        }
-      })
-  }
-  return []
 }
 
 export const getApplicationTimelineEvents = (
