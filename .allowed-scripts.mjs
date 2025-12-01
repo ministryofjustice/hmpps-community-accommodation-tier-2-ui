c0bb89c1d310e429f97d711f5c4475cd384f7af6
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-12-01 14:31:43 +0000

    Install and configure hmpps npm-script-allowlist

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,10 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+  allowlist: {
+    'node_modules/cypress@13.8.1': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
+  },
+})
