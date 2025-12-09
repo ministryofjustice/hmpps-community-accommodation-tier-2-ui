52f2395ae4bce9d788527707955d10eefc660581
AvraamK <avraam.kokkinis@justice.gov.uk>
2025-12-09 13:51:06 +0000

    Fix e2e tests

--- e2e-tests/steps/beforeYouStartSection.ts
+++ e2e-tests/steps/beforeYouStartSection.ts
@@ -13,10 +13,7 @@ export const completeEligibilityTask = async (page: Page, name: string) => {
 }
 
 export const completeConsentTask = async (page: Page, name: string) => {
-  const confirmConsentPage = await ApplyPage.initialize(
-    page,
-    `Confirm ${name}'s consent to apply for Short-Term Accommodation (CAS-2)`,
-  )
+  const confirmConsentPage = await ApplyPage.initialize(page, `Confirm ${name}'s consent to apply for CAS2 HDC`)
 
   await confirmConsentPage.checkRadio(`Yes, ${name} has given their consent`)
   await confirmConsentPage.fillDateFieldInGroup('When did they give consent?', {
