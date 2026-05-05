1d7f166bdb2560a98b7455ff2986da87f2a0a065
robanderson-moj <rob.anderson1@justice.gov.uk>
2026-05-05 11:49:02 +0100

    Update axios to resolve high severity vulnerability

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,7 +2,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@13.8.1': 'ALLOW',
+    'node_modules/cypress@13.17.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
