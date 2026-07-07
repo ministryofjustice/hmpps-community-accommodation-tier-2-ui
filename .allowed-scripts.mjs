5d9165c076f06f85c567b601c7f207e338e43f76
Bob Meredith <robert.meredith@digital.justice.gov.uk>
2026-07-07 09:24:18 +0100

    Update allowed-scripts

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -7,5 +7,7 @@ export default configureAllowedScripts({
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+    'node_modules/protobufjs@7.6.5': 'ALLOW',
+    'node_modules/shellcheck@1.1.0': 'ALLOW',
   },
 })
