1ac22fe0a643851a415a2665b3d8932e3c016843
AvraamK <avraam.kokkinis@justice.gov.uk>
2026-08-17 11:57:21 +0100

    Renaming of feature flag to the agreed format

--- server/utils/userUtils.test.ts
+++ server/utils/userUtils.test.ts
@@ -4,7 +4,7 @@ import config from '../config'
 describe('userUtils', () => {
   describe('sectionsForUser', () => {
     beforeEach(() => {
-      config.flags.hdcSunsetPhase1StopNewApplications = false
+      config.flags.phase1DisableApplicationCreation = false
     })
 
     it('should return an empty array for a user with no roles', () => {
@@ -31,9 +31,9 @@ describe('userUtils', () => {
       expect(sectionsForUser(['ROLE_CAS2_MI'])).toEqual(expected)
     })
 
-    describe('when the HDC sunset phase 1 stop new applications flag is enabled', () => {
+    describe('when the phase 1 disable application creation flag is enabled', () => {
       beforeEach(() => {
-        config.flags.hdcSunsetPhase1StopNewApplications = true
+        config.flags.phase1DisableApplicationCreation = true
       })
 
       it('should not return the new application section for a POM', () => {
